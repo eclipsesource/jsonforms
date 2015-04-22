58e92f781da3b4586bd51b630ccc5e8b5568a479
Stefan Dirix <sdirix@eclipsesource.com>
2015-04-22 18:37:02 +0200

    Add Gruntfile config to concat and minify via grunt

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Config for Concat Task
+        concat: {
+            options: {
+                // Remove all existing banners
+                stripBanners: true,
+
+                // Replace all 'use strict' statements in the code with a single one at the top
+                process: function(src, filepath) {
+                    return '// Source: ' + filepath + '\n' +
+                        src.replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1');
+                },
+
+                // Add new banner on top of generated file
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                '<%= grunt.template.today("yyyy-mm-dd") %> Copyright (c) EclipseSource Muenchen GmbH and others. */ \n' +
+                "'use strict';\n"
+            },
+            dist: {
+                // Concat all files from app/js directory
+                src: ['app/js/**'],
+                filter: 'isFile',
+                dest: 'bin/<%= pkg.name %>_<%= pkg.version %>.js'
+            }
+        },
+
+        // Config for Uglify (= Minify) Task
+        uglify: {
+            options: {
+                // Use default
+            },
+            build: {
+                src: 'bin/<%= pkg.name %>_<%= pkg.version %>.js',
+                dest: 'bin/<%= pkg.name %>_<%= pkg.version %>.min.js'
+            }
+        }
+    });
+
+    // Load the plugin that provides the "concat" task.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['concat', 'uglify']);
+
+};
