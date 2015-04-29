2f7cbe80a1b1ce6c9783652465b279fd4f83de82
Stefan Dirix <sdirix@eclipsesource.com>
2015-04-29 18:46:37 +0200

    Add Protractor Tests (e2e) to grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,12 +40,30 @@ module.exports = function(grunt) {
             }
         },
 
-        // Config for Karma (=Test) Task
+        // Config for Karma (Unit Test) Task
         karma: {
             unit: {
                 configFile: 'tests/unit-tests/karma.conf.js',
                 singleRun: true
             }
+        },
+
+        // Config for Connect (Start Webserver) Task
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    base: 'app'
+                }
+            }
+        },
+
+        // Config for Protractor (e2e Test) Task
+        protractor: {
+            options: {
+                configFile: "tests/e2e-tests/protractor.conf.js"
+            },
+            run: {}
         }
     });
 
@@ -58,7 +76,23 @@ module.exports = function(grunt) {
     // Load the plugin that provides the "karma" task.
     grunt.loadNpmTasks('grunt-karma');
 
+    // Load the plugin that provides the "connect" task.
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    // Load the plugin that provides the "protractor" task.
+    grunt.loadNpmTasks('grunt-protractor-runner');
+
     // Default task(s).
-    grunt.registerTask('default', ['karma', 'concat', 'uglify']);
+    grunt.registerTask('default', [
+        'karma',
+        'concat',
+        'uglify']);
+
+    // Test tasks
+    grunt.registerTask('test', [
+        'karma',
+        'connect',
+        'protractor'
+    ]);
 
 };
