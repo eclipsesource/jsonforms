9318b810249438200e27e0e71d9f7b5998f5bb60
Edgar Mueller <edgar.m.mueller@gmail.com>
2015-06-29 14:08:12 +0200

    Remove obsolete code, regenerated dist

--- dist/js/jsonforms.js
+++ dist/js/jsonforms.js
@@ -211,32 +211,6 @@ app.run(['RenderService', 'BindingService', 'ReferenceResolver',
         }
     });
 }]);
-// Source: js/widgets/data-send-service.js
-var module = angular.module('jsonForms.data.send', []);
-
-module.factory('SendData', ['$http', function($http) {
-    return {
-        sendData: function(url, type, id, data) {
-
-            if (id !== "") {
-                data.id = id;
-                console.log("post url is " + url + type + "/" + id);
-                $http.post(url + type + "/" + id, data).success(function() {
-                    alert("Update Data successful");
-                }).error(function(){
-                    alert("Update Data failed!");
-                });
-            } else {
-                $http.post("/" + type, data).success(function() {
-                    alert("Create Data successful");
-                }).error(function(){
-                    alert("Create Data failed!");
-                });
-            }
-        }
-    };
-}]);
-
 // Source: js/widgets/horizontal-layout.js
 
 var app = angular.module('jsonForms.horizontalLayout', []);
