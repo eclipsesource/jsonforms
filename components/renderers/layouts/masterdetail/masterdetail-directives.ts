5483d26f2ed85358f634d422190cafb2283caa1d
Eugen Neufeld <eneufeld@eclipsesource.com>
2016-03-03 15:38:22 +0100

    fixed masterdetails

--- components/renderers/layouts/masterdetail/masterdetail-directives.ts
+++ components/renderers/layouts/masterdetail/masterdetail-directives.ts
@@ -4,28 +4,39 @@ class MasterDetailCollectionDirective implements ng.IDirective {
     restrict = "E";
     replace = true;
     scope = {
-        collection: '=',
+        properties: '=',
+        instance: '=',
         element: '='
     };
     template = `
         <div>
             <accordion close-others="false">
-                <accordion-group is-open="status_attribute.open" ng-repeat="(key, value) in element.filter(collection)">
-                    <accordion-heading>
-                        {{key}} <i class="pull-right glyphicon" ng-class="{'glyphicon-chevron-down': status_attribute.open, 'glyphicon-chevron-right': !status_attribute.open}"></i>
+                <accordion-group is-open="status_attribute.open" ng-repeat="(key, value) in element.filter(properties)" ng-class="{'jsf-masterdetail-empty':isEmptyInstance(key)}">
+                    <accordion-heading class="jsf-masterdetail-header">
+                        <span class="jsf-masterdetail-property">{{key}}</span> <i class="pull-right glyphicon" ng-class="{'glyphicon-chevron-down': status_attribute.open, 'glyphicon-chevron-right': !status_attribute.open}" ng-show="!isEmptyInstance(key)"></i>
                     </accordion-heading>
-                    <accordion close-others="false">
-                        <accordion-group is-open="status_object.open" ng-repeat="child in element.instance[key]">
+
+                    <accordion close-others="false" ng-show="!isEmptyInstance(key)">
+                        <accordion-group is-open="status_object.open" ng-repeat="child in instance[key]" ng-class="{'jsf-masterdetail-empty':!hasKeys(value.items)}">
                             <accordion-heading>
-                                <span ng-click="element.selectedChild=child;element.selectedSchema=value.items;">{{child.name}}<!-- label provider needed --></span><i class="pull-right glyphicon" ng-class="{'glyphicon-chevron-down': status_object.open, 'glyphicon-chevron-right': !status_object.open}"></i>
+                                <span ng-click="element.selectedChild=child;element.selectedSchema=value.items;" ng-class="{'jsf-masterdetail-selected':element.selectedChild==child}">{{child.name!=undefined?child.name:child}}<!-- label provider needed --></span>
+                                <i class="pull-right glyphicon" ng-class="{'glyphicon-chevron-down': status_object.open, 'glyphicon-chevron-right': !status_object.open}" ng-if="hasKeys(value.items)"></i>
                             </accordion-heading>
-                            <jsonforms-masterdetail-member element="element" member="value.items"></jsonforms-masterdetail-member>
+                            <jsonforms-masterdetail-member element="element" child-schema="value.items" child-data="child"></jsonforms-masterdetail-member>
                         </accordion-group>
                     </accordion>
                 </accordion-group>
             </accordion>
         </div>
-        `
+        `;
+    link = (scope, element) => {
+        scope.hasKeys=function(schemaToCheck){
+            return Object.keys(scope.element.filter(schemaToCheck.properties)).length!=0;
+        };
+        scope.isEmptyInstance=function(key){
+            return scope.instance[key]==undefined || scope.instance[key].length==0;
+        }
+    };
 }
 
 // TODO (?) http://stackoverflow.com/questions/23535994/implementing-angularjs-directives-as-classes-in-typescript
@@ -37,14 +48,17 @@ class MasterDetailMember implements ng.IDirective {
     restrict = "E";
     replace = true;
     scope = {
-        member: '=',
-        element: '='
+        childSchema: '=',
+        childData: '=',
+        element: '=',
     };
     link = (scope, element) => {
-        this.$compile('<jsonforms-masterdetail-collection collection="member.properties" element="element"></jsonforms-masterdetail-collection>')
-        (scope, (cloned) => {
-            element.replaceWith(cloned);
-        });
+        if(Object.keys(scope.element.filter(scope.childSchema.properties)).length!=0){
+            this.$compile('<jsonforms-masterdetail-collection properties="childSchema.properties" element="element" instance="childData"></jsonforms-masterdetail-collection>')
+            (scope, (cloned) => {
+                element.replaceWith(cloned);
+            });
+        }
     }
 }
 
