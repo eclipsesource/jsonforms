de0eb63b339d0f133bf174a801df1b8648364fff
Edgar Mueller <edgar.m.mueller@gmail.com>
2016-06-14 18:05:28 +0200

    Fix app.js

--- examples/app/app.js
+++ examples/app/app.js
@@ -3,8 +3,4 @@
 angular.module('makeithappen', [
     'ngRoute',
     'jsonforms'
-        }).when('/resolve', {
-            templateUrl: 'app/resolve/resolve.html',
-            controller: 'ResolveController',
-            controllerAs: 'vm'
 ]);
