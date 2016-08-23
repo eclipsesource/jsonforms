7db1fb83d5141cdb51143367a1a455c84652da13
Edgar Mueller <emueller@eclipsesource.com>
2016-08-23 12:18:00 +0200

    Configure material build

---
+++ webpack/webpack.build-material.js
@@ -0,0 +1,90 @@
+var loaders = require("./loaders");
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: {
+        'dist/jsonforms-material': './src/material/jsonforms-material.ts',
+        'examples/assets/jsonforms': './src/material/jsonforms-material.ts'
+    },
+    output: {
+        filename: '[name].js',
+        path: './'
+    },
+    resolve: {
+        root: __dirname,
+        extensions: ['', '.ts', '.js', '.json']
+    },
+    resolveLoader: {
+        modulesDirectories: ["node_modules"]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(
+            {
+                warning: false,
+                mangle: true,
+                comments: false
+            }
+        ),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            'window.jquery': 'jquery'
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: 'src/jsonforms.css',
+                to:   'dist/jsonforms.css'
+            },
+            { 
+                from: 'src/bootstrap/jsonforms-bootstrap.css',
+                to:   'dist/jsonforms-bootstrap.css'
+            },
+            {
+                from: 'src/jsonforms.css',
+                to:   'examples/assets/jsonforms.css'
+            },
+            { 
+                from: 'src/bootstrap/jsonforms-bootstrap.css',
+                to:   'examples/assets/jsonforms-bootstrap.css'
+            },
+            {
+                from: 'src/bootstrap/jsf-bootstrap.css',
+                to:   'dist/bootstrap/jsf-bootstrap.css'
+            }
+        ])
+    ],
+    module: {
+        preLoaders: [
+            {
+                test: /\.ts$/,
+                loader: "tslint"
+            }
+        ],
+        loaders: loaders
+    },
+    tslint: {
+        // These options are useful if you want to save output to files
+        // for your continuous integration server
+        fileOutput: {
+            // The directory where each file's report is saved
+            dir: "./reports/tslint",
+
+            // The extension to use for each report's filename. Defaults to "txt"
+            ext: "xml",
+
+            // If true, all files are removed from the report directory at the beginning of run
+            clean: true,
+
+            // A string to include at the top of every report file.
+            // Useful for some report formats.
+            header: "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<checkstyle version=\"5.7\">",
+
+            // A string to include at the bottom of every report file.
+            // Useful for some report formats.
+            footer: "</checkstyle>"
+        }
+    }
+};
