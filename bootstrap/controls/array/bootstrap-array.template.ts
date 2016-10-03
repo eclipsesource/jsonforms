30b4874b020a05e3c7781ee22438d0c01aabc852
Stefan Dirix <sdirix@eclipsesource.com>
2016-10-03 17:28:17 +0200

    Add Glyphicon-Styled Array Delete Button in Bootstrap

--- bootstrap/controls/array/bootstrap-array.template.ts
+++ bootstrap/controls/array/bootstrap-array.template.ts
@@ -4,13 +4,35 @@ const arrayTemplate =  `
   <legend>{{vm.label}}</legend>
   <div>
     <div ng-repeat="d in vm.resolvedData" ng-if="vm.fragment === undefined" class="well well-sm">
-      <jsonforms schema="vm.arraySchema" data="d" uischema="vm.arrayUiSchema"></jsonforms>
+      <div class="row jsf-control-array-container">
+        <div class="col-sm-90 jsf-control-array-element">
+          <jsonforms schema="vm.arraySchema" data="d" uischema="vm.arrayUiSchema"></jsonforms>
+        </div>
+        <div class="col-sm-10 jsf-control-array-element-delete">
+          <button class="btn btn-default"
+               ng-show="vm.supportsDelete"
+               ng-click="vm.deleteCallback(d)">
+               <span class="glyphicon glyphicon-remove-circle"></span>
+          </button>
+        </div>
+      </div>
     </div>
     <div ng-repeat="d in vm.resolvedData[vm.fragment]"
       ng-if="vm.fragment !== undefined" class="well well-sm">
-      <jsonforms schema="vm.arraySchema" data="d" uischema="vm.arrayUiSchema"></jsonforms>
+      <div class="row jsf-control-array-container">
+        <div class="col-sm-90 jsf-control-array-element">
+          <jsonforms schema="vm.arraySchema" data="d" uischema="vm.arrayUiSchema"></jsonforms>
+        </div>
+        <div class="col-sm-10 jsf-control-array-element-delete">
+          <button class="btn btn-default"
+               ng-show="vm.supportsDelete"
+               ng-click="vm.deleteCallback(d)">
+               <span class="glyphicon glyphicon-remove-circle"></span>
+          </button>
+        </div>
+      </div>
     </div>
-    <div ng-if="vm.isEmpty" class="readonly-array-empty">{{vm.emptyMsg}}</div>
+    <div ng-if="vm.isEmpty" class="array-empty">{{vm.emptyMsg}}</div>
     <input class="btn btn-primary"
            ng-show="vm.supportsSubmit"
            type="button"
