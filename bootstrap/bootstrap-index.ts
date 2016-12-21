e6e69a51f9849c18d43db0cb9aa79a59f6b899c2
Eugen Neufeld <eneufeld@eclipsesource.com>
2016-12-21 16:57:23 +0100

    reverted bootstrap changes

--- bootstrap/bootstrap-index.ts
+++ bootstrap/bootstrap-index.ts
@@ -3,7 +3,6 @@ import form from '../src/components/form/form';
 import ngServices from '../src/components/ng-services/ng-services';
 import jsonformsBootstrap from './jsonforms-bootstrap';
 require('angular-ui-validate');
-require('./jsonforms-bootstrap.css');
 
 export default angular.module('jsonforms', [
     'ui.validate',
