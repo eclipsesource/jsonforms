8a661ec6600f8594115814517ff5f726ba85809f
Mat Hansen <mhansen@eclipsesource.com>
2017-08-01 11:49:29 +0200

    Add initial model mapping test case

--- test/modelmapping.test.ts
+++ test/modelmapping.test.ts
@@ -1,96 +1,106 @@
 import test from 'ava';
 import { JsonForms } from '../src/core';
+import { testDataSchema } from './data/modelreference.data';
+import { SchemaService } from '../src/core/schema.service';
+import { SchemaServiceImpl } from '../src/core/schema.service.impl';
 
-test('get available options for reference attribute', t => {
+test.beforeEach(t => {
 
-    const classA = {
-      "eClass": "http://www.eclipse.org/emf/2002/Ecore#//EClass",
-      "name": "classA",
-      "_id" : "id-class-a",
+  const classA = {
+    "eClass": "http://www.eclipse.org/emf/2002/Ecore#//EClass",
+    "name": "classA",
+    "_id" : "id-class-a",
+    "eStructuralFeatures": [
+      {
+        "eClass": "http://www.eclipse.org/emf/2002/Ecore#//EAttribute",
+        "eType": {},
+        "name": "attributeOne"
+      },
+      {
+        "eClass": "http://www.eclipse.org/emf/2002/Ecore#//EAttribute",
+        "eType": {},
+        "name": "attributeTwo"
+      },
+      {
+        "eClass": "http://www.eclipse.org/emf/2002/Ecore#//EReference",
+        "name": "referenceOne",
+        "eType": "id-class-b"
+      }
+    ]
+  };
+
+  const classB = {
+    "eClass": "http://www.eclipse.org/emf/2002/Ecore#//EClass",
+      "name": "classB",
+      "_id": "id-class-b",
       "eStructuralFeatures": [
         {
           "eClass": "http://www.eclipse.org/emf/2002/Ecore#//EAttribute",
           "eType": {},
           "name": "attributeOne"
         },
-        {
-          "eClass": "http://www.eclipse.org/emf/2002/Ecore#//EAttribute",
-          "eType": {},
-          "name": "attributeTwo"
-        },
-        {
-          "eClass": "http://www.eclipse.org/emf/2002/Ecore#//EReference",
-          "name": "referenceOne",
-          "eType": "id-class-b"
-        }
       ]
-    };
+  }
 
-    const classB = {
-      "eClass": "http://www.eclipse.org/emf/2002/Ecore#//EClass",
-        "name": "classB",
-        "_id": "id-class-B",
-        "eStructuralFeatures": [
-          {
-            "eClass": "http://www.eclipse.org/emf/2002/Ecore#//EAttribute",
-            "eType": {},
-            "name": "attributeOne"
-          },
-        ]
-    }
-
-    const enumOne = {
-      "eClass": "http://www.eclipse.org/emf/2002/Ecore#//EEnum",
-        "name": "enumOne",
-        "_id": "id-enum-one",
-        "eLiterals": [
-            "enumLiteralOne", "enumLiteralTwo", "enumLiteralThree"
-        ]
-    }
-
-    const dataTypeOne = {
-      "eClass": "http://www.eclipse.org/emf/2002/Ecore#//EDataType",
-       "name": "dataTypeOne",
-       "_id": "id-datatype-one",
-       "instanceClassName" : "dataTypeOne",
-       "instanceTypeName" : "java.lang.String"
-    }
-
-    // data has to consist of a bunch of eClassifiers
-    // so we can verify whether the filtering works
-    const data = {
-      "name": "packageOne",
-      "eClassifiers": [
-        classA,
-        classB,
-        enumOne,
-        dataTypeOne
+  const enumOne = {
+     "eClass": "http://www.eclipse.org/emf/2002/Ecore#//EEnum",
+      "name": "enumOne",
+      "_id": "id-enum-one",
+      "eLiterals": [
+          "enumLiteralOne", "enumLiteralTwo", "enumLiteralThree"
       ]
-    };
+  }
 
-    JsonForms.modelMapping = {
-      'attribute': 'eClass',
-      'mapping': {
-        'http://www.eclipse.org/emf/2002/Ecore#//EEnum': '#enum',
-        'http://www.eclipse.org/emf/2002/Ecore#//EClass': '#class',
-        'http://www.eclipse.org/emf/2002/Ecore#//EDataType': '#datatype',
-        'http://www.eclipse.org/emf/2002/Ecore#//EReference': '#reference',
-        'http://www.eclipse.org/emf/2002/Ecore#//EAttribute': '#attribute'
-      }
-    };
-    const candidates = [
-        classA,
-        classB,
-        enumOne,
-        dataTypeOne
-      ];
+  const dataTypeOne = {
+    "eClass": "http://www.eclipse.org/emf/2002/Ecore#//EDataType",
+     "name": "dataTypeOne",
+     "_id": "id-datatype-one",
+     "instanceClassName" : "dataTypeOne",
+     "instanceTypeName" : "java.lang.String"
+  }
 
-    // test filtering
-    const matchingOptions = JsonForms.filterObjectsByType(candidates, "#class");
-    t.is(Object.keys(matchingOptions).length, 2, "array of length two expected");
+  t.context.candidates = [
+      classA,
+      classB,
+      enumOne,
+      dataTypeOne
+    ];
 
-    // TODO: test retun of all available options
-    // const allOptions = JsonForms.filterObjectsByType(candidates, null);
-    // t.is(Object.keys(allOptions).length, 4, "array of length four expected");
+  // data has to consist of a bunch of eClassifiers
+  // so we can verify whether the filtering works
+  t.context.data = {
+    "name": "packageOne",
+    "eClassifiers": t.context.candidates
+  }
+
+  t.context.modelMapping = {
+    'attribute': 'eClass',
+    'mapping': {
+      'http://www.eclipse.org/emf/2002/Ecore#//EEnum': '#enum',
+      'http://www.eclipse.org/emf/2002/Ecore#//EClass': '#class',
+      'http://www.eclipse.org/emf/2002/Ecore#//EDataType': '#datatype',
+      'http://www.eclipse.org/emf/2002/Ecore#//EReference': '#reference',
+      'http://www.eclipse.org/emf/2002/Ecore#//EAttribute': '#attribute'
+    }
+  };
+  JsonForms.modelMapping = t.context.modelMapping;
+
+});
+
+test('available options filtering for reference attribute candidates', t => {
+
+  const matchingOptions = JsonForms.filterObjectsByType(t.context.candidates, "#class");
+
+  t.is(Object.keys(matchingOptions).length, 2, "array of length two expected");
+
+});
+
+test('available options filtering for reference attribute', t => {
+
+  const service: SchemaService = new SchemaServiceImpl(testDataSchema);
+  const reference = service.getReferenceProperties(testDataSchema.definitions.reference)[0];
+  const availableOptions = reference.findReferenceTargets(t.context.data);
+
+  t.is(Object.keys(availableOptions).length, 2, "array of length two expected");
 
 });
