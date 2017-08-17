0047991062dd13a1429607523431d4c1ca6189db
Lucas Koehler <l.koehler93@gmail.com>
2017-08-17 11:26:08 +0200

    Removed unnecesary code from the reference control's test implementation

--- test/renderers/reference.control.testimpl.ts
+++ test/renderers/reference.control.testimpl.ts
@@ -1,19 +1,5 @@
 import { ReferenceControl } from '../../src/renderers/controls/reference.control';
-// import { and, RankedTester, rankWith, schemaTypeIs, uiTypeIs } from '../../src/core/testers';
-// import { JsonFormsRenderer } from '../../src/renderers/renderer.util';
 
-// export const booleanControlTester: RankedTester = rankWith(2, and(
-//     uiTypeIs('Control'),
-//     schemaTypeIs('boolean')
-//   ));
-//
-// /**
-//  * Default boolean control.
-//  */
-// @JsonFormsRenderer({
-//   selector: 'jsonforms-boolean',
-//   tester: booleanControlTester
-// })
 export class ReferenceControlTestImpl extends ReferenceControl {
   private rootData: Object;
   private labelProperty: string;
