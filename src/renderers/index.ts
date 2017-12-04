6c0599cdb2204344107d9cf1fa763f10ee6e552e
Eugen Neufeld <eneufeld@eclipsesource.com>
2017-12-04 10:26:27 +0100

    Removed array-control and updated table control
    This improves the test cases and the css for the table control.

--- src/renderers/index.ts
+++ src/renderers/index.ts
@@ -6,7 +6,6 @@ export * from './controls/input.control';
 export * from './layouts/vertical.layout';
 export * from './layouts/horizontal.layout';
 export * from './layouts/group.layout';
-export * from './additional/array-renderer';
 export * from './additional/tree-renderer';
 export * from './additional/categorization-renderer';
 export * from './additional/table-array.control';
