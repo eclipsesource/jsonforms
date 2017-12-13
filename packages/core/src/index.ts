c3905da7205f78c8043a2c0e90c4238319180400
Edgar Mueller <emueller@eclipsesource.com>
2017-12-13 19:03:44 +0100

    Fix issue #696: Define API
    * Provide Test and Resolve util objects for alternative access

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,5 @@
+import { JsonSchema } from './models/jsonSchema';
+
 export * from './core';
 export * from './json-forms';
 export * from './path.util';
@@ -5,7 +7,7 @@ export * from './core/renderer';
 export * from './core/runtime';
 export * from './core/schema.service';
 export * from './core/styling.registry';
-export * from './models/uischema';
+
 export * from './models/jsonSchema';
 export * from './store';
 export *  from './actions';
@@ -13,9 +15,20 @@ export *  from './renderers';
 export * from './reducers';
 export * from './generators';
 
-// do not export findAllRefs and ReferenceSchemaMap
-export { resolveData, resolveSchema } from './resolvers';
+export * from './models/uischema';
 
-import * as Testers from './testers';
+// do not export findAllRefs and ReferenceSchemaMap
+import { resolveData, resolveSchema } from './resolvers';
+const Resolve: {
+  schema(schema: JsonSchema, schemaPath: string): JsonSchema;
+  data(data, path): any
+} = {
+  schema: resolveSchema,
+  data: resolveData
+};
+export { resolveData, resolveSchema } from './resolvers';
+export { Resolve };
+
+import * as Test from './testers';
 export * from './testers';
-export { Testers };
+export { Test };
