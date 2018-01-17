0047454f7abb7909045d8b97129c7bd08b71c8b9
Eugen Neufeld <eneufeld@eclipsesource.com>
2018-01-17 14:30:02 +0100

    fixed rename

--- packages/examples/src/day6.ts
+++ packages/examples/src/day6.ts
@@ -1,117 +1,21 @@
 import { registerExamples } from './register';
+import { connect } from 'react-redux';
 import {
   JsonFormsElement,
+  mapStateToControlProps,
   registerRenderer,
   unregisterRenderer
 } from '@jsonforms/core';
 import ConnectedRatingControl, { ratingControlTester } from './rating.control';
-const schema = {
-  'type': 'object',
-  'properties': {
-    'name': {
-      'type': 'string'
-    },
-    'description': {
-      'type': 'string'
-    },
-    'done': {
-      'type': 'boolean'
-    },
-    'due_date': {
-      'type': 'string',
-      'format': 'date'
-    },
-    'rating': {
-      'type': 'integer',
-      'maximum': 5
-    },
-    'recurrence': {
-        'type': 'string',
-        'enum': ['Never', 'Daily', 'Weekly', 'Monthly']
-    },
-    'recurrence_interval': {
-        'type': 'integer'
-    }
-  },
-  'required': ['name']
-};
-const uischema = {
-  'type': 'VerticalLayout',
-  'elements': [
-    {
-      'type': 'Control',
-      'label': false,
-      'scope': {
-        '$ref': '#/properties/done'
-      }
-    },
-    {
-      'type': 'Control',
-      'scope': {
-        '$ref': '#/properties/name'
-      }
-    },
-    {
-      'type': 'HorizontalLayout',
-      'elements': [
-        {
-          'type': 'Control',
-          'scope': {
-            '$ref': '#/properties/due_date'
-          }
-        },
-        {
-          'type': 'Control',
-          'scope': {
-            '$ref': '#/properties/rating'
-          }
-        }
-      ]
-    },
-    {
-      'type': 'Control',
-      'scope': {
-        '$ref': '#/properties/description'
-      },
-      'options': {
-          'multi': true
-      }
-    },
-    {
-      'type': 'HorizontalLayout',
-      'elements': [
-        {
-          'type': 'Control',
-          'scope': {
-            '$ref': '#/properties/recurrence'
-          }
-        },
-        {
-          'type': 'Control',
-          'scope': {
-            '$ref': '#/properties/recurrence_interval'
-          },
-          'rule': {
-              'effect': 'HIDE',
-              'condition': {
-                  'scope': {
-                      '$ref': '#/properties/recurrence'
-                  },
-                  'expectedValue': 'Never'
-              }
-          }
-        }
-      ]
-    }
-  ]
-};
-const data =  {
-  'name': 'Send email to Adriana',
-  'description': 'Confirm if you have passed the subject\nHerby ...',
-  'done': true,
-  'recurrence': 'Daily',
-  'rating': 3,
-};
+import {
+  data as day5Data,
+  schema as day5Schema,
+  uischema as day5UiSchema,
+} from './day5';
+
+export const schema = day5Schema;
+export const uischema = day5UiSchema;
+export const data = day5Data;
 
 const setup = (div: HTMLDivElement) => {
   const buttonRegister = document.createElement('button');
