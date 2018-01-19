2970c35145d220f2476033ffb6bdf9489ec362d7
Edgar Mueller <emueller@eclipsesource.com>
2018-01-19 11:27:01 +0100

    Remove tree renderer

--- packages/vanilla/src/additional/tree/index.ts
+++
@@ -1,8 +0,0 @@
-import { registerStartupRenderer } from '@jsonforms/core';
-import TreeRenderer from './TreeRenderer';
-import { treeMasterDetailTester } from './tester';
-
-export default registerStartupRenderer(
-  treeMasterDetailTester,
-  TreeRenderer
-);
