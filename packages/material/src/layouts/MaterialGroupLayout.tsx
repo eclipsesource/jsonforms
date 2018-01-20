6657a26b71939617353a11119f7e6cd6ab2dfbb9
ASE-Student <ga97waq@mytum.de>
2018-01-20 17:34:15 +0100

    changes to fullfill merge request

--- packages/material/src/layouts/MaterialGroupLayout.tsx
+++ packages/material/src/layouts/MaterialGroupLayout.tsx
@@ -1,47 +1,44 @@
 import Card from 'material-ui/Card';
+import CardHeader from 'material-ui/Card';
 import * as React from 'react';
-import * as _ from 'lodash';
 import {
     GroupLayout,
     JsonForms,
     mapStateToLayoutProps,
+    RankedTester,
+    rankWith,
     registerStartupRenderer,
-    renderChildren,
     RendererProps,
+    uiTypeIs,
     withIncreasedRank
 } from '@jsonforms/core';
 import { connect } from 'react-redux';
-import { groupTester } from './group.layout';
+import { MaterialLayoutRenderer, MaterialLayoutRendererProps } from './layout.util';
+
+export const groupTester: RankedTester = rankWith(1, uiTypeIs('Group'));
 
 export const MaterializedGroupLayoutRenderer = (props: RendererProps) => {
-    const { uischema, schema, path } = props;
+    const { uischema, schema, path, visible } = props;
 
-    const group = uischema as GroupLayout;
+    const groupLayout = uischema as GroupLayout;
     const classNames = JsonForms.stylingRegistry.getAsClassName('group-layout');
 
+    const childProps: MaterialLayoutRendererProps = {
+        elements: groupLayout.elements,
+        schema,
+        path,
+        direction: 'column',
+        visible
+    };
+
     return (
-            <div style={ {marginBottom: '10px' }}>
-                <Card className={classNames}
-                    style={{ padding: '10px 12px 10px 12px',
-                        color: 'rgba(0, 0, 0, 0.54)' }}>
-                    {
-                        !_.isEmpty(group.label) ?
-                            <div className={JsonForms.stylingRegistry.getAsClassName
-                            ('group.label')}
-                            style={{ marginBottom: '15px' }}>
-                                {group.label}
-                            </div> : ''
-                    }
-                    {
-                        renderChildren(
-                            group.elements,
-                            schema,
-                            'group-layout-item',
-                            path
-                        )
-                    }
-                </Card>
-            </div>
+        <Card className={classNames} style={{marginBottom: '10px' }}>
+            !_.isEmpty(groupLayout.label) ?
+                <CardHeader>
+                    {groupLayout.label}
+                </CardHeader>
+            <MaterialLayoutRenderer {...childProps}/>;
+        </Card>
         );
 };
 
