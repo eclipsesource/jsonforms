ba96618bb5ac289544ff5724ca50d37071162e29
Eugen Neufeld <eneufeld@eclipsesource.com>
2018-01-20 20:56:47 +0100

    moved jsonforms test

---
+++ packages/webcomponent/test/FakeControl.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import {
+  ControlElement,
+  ControlProps,
+  convertToValidClassName,
+  isControl,
+  mapStateToControlProps,
+  RankedTester,
+  rankWith,
+  registerStartupRenderer
+} from '@jsonforms/core';
+import { connect } from 'react-redux';
+
+/**
+ * Default tester for a horizontal layout.
+ * @type {RankedTester}
+ */
+export const fakeTester: RankedTester = rankWith(2, isControl);
+
+const FakeControl = (props: ControlProps) => {
+
+  const {uischema} = props;
+  const controlElement = uischema as ControlElement;
+
+  return (
+    <div className={convertToValidClassName(controlElement.scope.$ref)}/>
+  );
+};
+
+export default registerStartupRenderer(
+    fakeTester,
+    connect(mapStateToControlProps, null)(FakeControl)
+);
