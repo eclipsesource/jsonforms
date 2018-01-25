d20c0ed0f156fad005e7b837e765f2590077892d
Daniel Kutasi <daniel.kutasi@outlook.de>
2018-01-25 19:25:09 +0100

    fixes npm run dev after 5486d6c3

--- packages/vanilla/example/index.ts
+++ packages/vanilla/example/index.ts
@@ -1,6 +1,6 @@
 import { createExampleSelection } from '../../examples/src/register';
 import { createThemeSelection } from './theme.switcher';
-import { vanillaStyles } from '../src/helpers';
+import { vanillaStyles } from '../src/util';
 
 window.onload = () => {
   createExampleSelection({styles: vanillaStyles});
