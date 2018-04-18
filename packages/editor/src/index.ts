e1c4df65e4bf26950e8484f78c5b8578ebb8bd6f
Lucas Koehler <lkoehler@eclipsesource.com>
2018-04-18 17:33:36 +0200

    [Editor] Export missing stuff
    * Properly export the tree renderer
    * Export editor reducers/selectors
    * Export SchemaService

--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -4,3 +4,9 @@ export * from './master-detail-layout';
 export * from './editor';
 export * from './tree/TreeRenderer';
 export * from './helpers/util';
+export * from './reducers/index';
+
+// TODO remove the following exports when they are not needed anymore
+export * from './editor-context';
+export * from './services/schema.service';
+export * from './services/schema.service.impl';
