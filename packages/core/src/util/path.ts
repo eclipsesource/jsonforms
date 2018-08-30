c462091413ffe951ef33c6eb7d880253028a52dc
Edgar Mueller <emueller@eclipsesource.com>
2018-08-30 15:14:33 +0200

    Add material-based anyOf renderer
    * Make toDataPathSegments be based on filtering of keywords

--- packages/core/src/util/path.ts
+++ packages/core/src/util/path.ts
@@ -51,13 +51,16 @@ export const compose = (path1: string, path2: string) => {
  * @param {string} schemaPath the schema path to be converted
  * @returns {string[]} an array containing only non-schema-specific segments
  */
+const keywords = ['properties', 'items', '#'];
+
 export const toDataPathSegments = (schemaPath: string): string[] => {
-
-  const segments = schemaPath.split('/');
-  const startFromRoot = segments[0] === '#' || segments[0] === '';
-  const startIndex =  startFromRoot ? 2 : 1;
-
-  return _.range(startIndex, segments.length, 2).map(idx => segments[idx]);
+  const segments = schemaPath
+      .replace(/anyOf\/[\d]/, '')
+      .replace(/allOf\/[\d]/, '')
+      .split('/');
+  return segments
+      .filter(p => keywords.indexOf(p) === -1)
+      .filter(s => !_.isEmpty(s));
 };
 
 /**
