bcf8c55e57845a9ec006576703e0ae2536b4cd92
Edgar Mueller <emueller@eclipsesource.com>
2018-10-08 15:50:08 +0200

    [i18n] Remove implicit any warnings

--- packages/i18n/src/reducers/i18n.ts
+++ packages/i18n/src/reducers/i18n.ts
@@ -1,19 +1,19 @@
 /*
   The MIT License
-  
+
   Copyright (c) 2018 EclipseSource Munich
   https://github.com/eclipsesource/jsonforms
-  
+
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
   in the Software without restriction, including without limitation the rights
   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
   copies of the Software, and to permit persons to whom the Software is
   furnished to do so, subject to the following conditions:
-  
+
   The above copyright notice and this permission notice shall be included in
   all copies or substantial portions of the Software.
-  
+
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,7 +28,7 @@ export const i18nReducer = (
   state = {
     locale: navigator.languages[0]
   },
-  action) => {
+  action: any) => {
   switch (action.type) {
     case SET_TRANSLATIONS:
       return {
@@ -45,8 +45,8 @@ export const i18nReducer = (
   }
 };
 
-export const fetchTranslation = state => {
+export const fetchTranslation = (state: any/*JsonFormsExtendedState*/) => {
   return state.translations ? state.translations[state.locale] : undefined;
 };
 
-export const fetchLocale = state => state.locale;
+export const fetchLocale = (state: any) => state.locale;
