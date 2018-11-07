452327be41440ebd0c74a4040a1b4ef7660c206d
Edgar Mueller <emueller@eclipsesource.com>
2018-11-07 18:39:45 +0100

    [material] Add table cell with disabled bottom border

---
+++ packages/material/src/complex/NoBorderTableCell.tsx
@@ -0,0 +1,17 @@
+import { withStyles } from '@material-ui/core/styles';
+import { TableCell } from '@material-ui/core';
+import * as React from 'react';
+
+const styles = {
+    noBottomBorder: {
+        borderBottom: 'none'
+    }
+};
+
+const NoBorderTableCell = ({ classes, children, ...otherProps }: any) => (
+    <TableCell className={classes.noBottomBorder} {...otherProps}>
+        {children}
+    </TableCell>
+);
+
+export default withStyles(styles)(NoBorderTableCell);
