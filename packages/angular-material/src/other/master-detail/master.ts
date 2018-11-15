af94f5a51189b2a8fc7fdf2ad126a2a4c2af5d24
Edgar Mueller <emueller@eclipsesource.com>
2018-11-15 20:24:17 +0100

    [angular-material] Add add/delete buttons to list-with-detail renderer

--- packages/angular-material/src/other/master-detail/master.ts
+++ packages/angular-material/src/other/master-detail/master.ts
@@ -3,26 +3,27 @@ import * as _ from 'lodash';
 import { JsonFormsControl } from '@jsonforms/angular';
 import { NgRedux } from '@angular-redux/store';
 import {
-    ControlElement,
-    ControlProps,
-    Generate,
-    JsonFormsState,
-    RankedTester,
-    rankWith,
-    resolveSchema,
-    toDataPath,
-    uiTypeIs
+  ControlElement,
+  ControlProps,
+  Generate,
+  JsonFormsState,
+  mapDispatchToArrayControlProps,
+  RankedTester,
+  rankWith,
+  resolveSchema,
+  toDataPath,
+  uiTypeIs
 } from '@jsonforms/core';
 
 const keywords = ['#', 'properties', 'items'];
 
 export const removeSchemaKeywords = (path: string) => {
-    return path.split('/').filter(s => !_.some(keywords, key => key === s)).join('.');
+  return path.split('/').filter(s => !_.some(keywords, key => key === s)).join('.');
 };
 
 @Component({
-    selector: 'jsonforms-list-with-detail-master',
-    template: `
+  selector: 'jsonforms-list-with-detail-master',
+  template: `
     <mat-sidenav-container class="container" [fxHide]="hidden">
         <mat-sidenav mode="side" opened>
           <mat-nav-list>
@@ -32,64 +33,95 @@ export const removeSchemaKeywords = (path: string) => {
                 (click)="onSelect(item)"
             >
                 <a matLine>{{item.label}}</a>
+                <button mat-icon-button class="button" (click)="onDeleteClick(item)">
+                  <mat-icon mat-list-icon>delete</mat-icon>
+                </button>
             </mat-list-item>
           </mat-nav-list>
+           <button mat-fab color="primary" class="button" (click)="onAddClick()">
+               <mat-icon aria-label="Add item to list">add</mat-icon>
+           </button>
         </mat-sidenav>
         <mat-sidenav-content class="content">
           <jsonforms-detail [item]="selectedItem"></jsonforms-detail>
         </mat-sidenav-content>
     </mat-sidenav-container>`,
-    styles: [`
+  styles: [`
       .container {
         height: 100vh;
       }
       .content {
         padding: 15px;
       }
+      .button {
+        float: right;
+        margin-right: 0.25em;
+      }
     `]
 })
 export class MasterListComponent extends JsonFormsControl {
 
-    masterItems: any[];
-    selectedItem: any;
+  masterItems: any[];
+  selectedItem: any;
+  addItem: (path: string) => () => void;
+  removeItems: (path: string, toDelete: any[]) => () => void;
+  propsPath: string;
 
-    constructor(ngRedux: NgRedux<JsonFormsState>) {
-        super(ngRedux);
-    }
+  constructor(ngRedux: NgRedux<JsonFormsState>) {
+    super(ngRedux);
+  }
 
-    trackElement(_index: number, element: any) {
-        return element ? element.label : null;
-      }
+  trackElement(_index: number, element: any) {
+    return element ? element.label : null;
+  }
 
-    mapAdditionalProps(props: ControlProps) {
-        const { data, schema, uischema} = props;
-        const controlElement = uischema as ControlElement;
-        const instancePath = toDataPath(`${controlElement.scope}/items`);
-        const resolvedSchema = resolveSchema(schema, `${controlElement.scope}/items`);
-        const detailUISchema = controlElement.options.detail ||
-            Generate.uiSchema(resolvedSchema, 'VerticalLayout');
-        const masterItems = data.map((d: any, index: number) => {
-            const labelRefInstancePath =
-                removeSchemaKeywords(controlElement.options.labelRef);
-            const masterItem = {
-                label: _.get(d, labelRefInstancePath),
-                data: d,
-                path: `${instancePath}.${index}`,
-                schema: resolvedSchema,
-                uischema: detailUISchema
-            };
-            return masterItem;
-        });
+  ngOnInit() {
+    super.ngOnInit();
+    const { addItem, removeItems } = mapDispatchToArrayControlProps(this.ngRedux.dispatch, {
+      uischema: this.uischema as ControlElement,
+      schema: this.schema,
+    });
+    this.addItem = addItem;
+    this.removeItems = removeItems;
+  }
 
-        this.masterItems = masterItems;
-    }
+  mapAdditionalProps(props: ControlProps) {
+    const { data, schema, uischema } = props;
+    const controlElement = uischema as ControlElement;
+    const instancePath = toDataPath(`${controlElement.scope}/items`);
+    this.propsPath = props.path;
+    const resolvedSchema = resolveSchema(schema, `${controlElement.scope}/items`);
+    const detailUISchema = controlElement.options.detail ||
+      Generate.uiSchema(resolvedSchema, 'VerticalLayout');
+    const masterItems = (data || []).map((d: any, index: number) => {
+      const labelRefInstancePath =
+        removeSchemaKeywords(controlElement.options.labelRef);
+      const masterItem = {
+        label: _.get(d, labelRefInstancePath),
+        data: d,
+        path: `${instancePath}.${index}`,
+        schema: resolvedSchema,
+        uischema: detailUISchema
+      };
+      return masterItem;
+    });
+    this.masterItems = masterItems;
+  }
 
-    onSelect(item: any): void {
-        this.selectedItem = item;
-    }
+  onSelect(item: any): void {
+    this.selectedItem = item;
+  }
+
+  onAddClick() {
+    this.addItem(this.propsPath)();
+  }
+
+  onDeleteClick(item: any) {
+    this.removeItems(this.propsPath, [item.data])();
+  }
 }
 
 export const masterDetailTester: RankedTester = rankWith(
-    4,
-    uiTypeIs('ListWithDetail')
+  4,
+  uiTypeIs('ListWithDetail')
 );
