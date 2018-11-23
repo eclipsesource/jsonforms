11425cb20dab47ac1f8dd4f0a5641aa80363d15f
Edgar Mueller <emueller@eclipsesource.com>
2018-11-23 14:16:02 +0100

    [angular-material] ListWithDetail improvements * Implement default selection behavior * Fixed with of master sidenav * Delete buttons only shown on hover

--- packages/angular-material/src/other/master-detail/master.ts
+++ packages/angular-material/src/other/master-detail/master.ts
@@ -27,23 +27,30 @@ export const removeSchemaKeywords = (path: string) => {
     <mat-sidenav-container class="container" [fxHide]="hidden">
         <mat-sidenav mode="side" opened>
           <mat-nav-list>
+            <mat-list-item *ngIf="masterItems.length === 0">No items</mat-list-item>
             <mat-list-item
-                *ngFor="let item of masterItems; trackBy: trackElement"
+                *ngFor="let item of masterItems; let i = index; trackBy: trackElement"
                 [class.selected]="item === selectedItem"
-                (click)="onSelect(item)"
+                (click)="onSelect(item, i)"
+                (mouseover)="onListItemHover(i)"
+                (mouseout)="onListItemHover(undefined)"
             >
                 <a matLine>{{item.label}}</a>
-                <button mat-icon-button class="button" (click)="onDeleteClick(item)">
+                <button mat-icon-button
+                        class="button hide"
+                        (click)="onDeleteClick(item, i)"
+                        [ngClass]="{'show': highlightedIdx == i}"
+                >
                   <mat-icon mat-list-icon>delete</mat-icon>
                 </button>
             </mat-list-item>
           </mat-nav-list>
-           <button mat-fab color="primary" class="button" (click)="onAddClick()">
+           <button mat-fab color="primary" class="add-button" (click)="onAddClick()" >
                <mat-icon aria-label="Add item to list">add</mat-icon>
            </button>
         </mat-sidenav>
         <mat-sidenav-content class="content">
-          <jsonforms-detail [item]="selectedItem"></jsonforms-detail>
+          <jsonforms-detail *ngIf="selectedItem" [item]="selectedItem"></jsonforms-detail>
         </mat-sidenav-content>
     </mat-sidenav-container>`,
   styles: [`
@@ -53,9 +60,23 @@ export const removeSchemaKeywords = (path: string) => {
       .content {
         padding: 15px;
       }
+      .add-button {
+        float: right;
+        margin-top: 0.5em;
+        margin-right: 0.25em;
+      }
       .button {
         float: right;
         margin-right: 0.25em;
+      }
+      .hide {
+        display: none;
+      }
+      .show {
+        display: inline-block;
+      }
+      mat-sidenav {
+        width: 20%;
       }
     `]
 })
@@ -63,12 +84,18 @@ export class MasterListComponent extends JsonFormsControl {
 
   masterItems: any[];
   selectedItem: any;
+  selectedItemIdx: number;
   addItem: (path: string) => () => void;
   removeItems: (path: string, toDelete: any[]) => () => void;
   propsPath: string;
+  highlightedIdx: number;
 
   constructor(ngRedux: NgRedux<JsonFormsState>) {
     super(ngRedux);
+  }
+
+  onListItemHover(idx: number) {
+    this.highlightedIdx = idx;
   }
 
   trackElement(_index: number, element: any) {
@@ -106,17 +133,45 @@ export class MasterListComponent extends JsonFormsControl {
       return masterItem;
     });
     this.masterItems = masterItems;
+
+    // pre-select 1st entry
+    if (this.masterItems.length > 0 && this.selectedItem === undefined) {
+      this.selectedItem = this.masterItems[0];
+      this.selectedItemIdx = 0;
+    }
   }
 
-  onSelect(item: any): void {
+  onSelect(item: any, idx: number): void {
     this.selectedItem = item;
+    this.selectedItemIdx = idx;
   }
 
   onAddClick() {
     this.addItem(this.propsPath)();
   }
 
-  onDeleteClick(item: any) {
+  onDeleteClick(item: any, idx: number) {
+    this.deleteItem(item);
+    if (this.selectedItem.path === item.path && this.masterItems[idx]) {
+      // select next element, if possible
+      this.selectedItem = this.masterItems[idx];
+      this.selectedItemIdx = idx;
+    } else if (this.selectedItem.path === item.path && this.masterItems.length > 0) {
+      // select 1st entry, if no next element available
+      this.selectedItem = this.masterItems[0];
+      this.selectedItemIdx = 0;
+    } else if (this.selectedItemIdx >= idx) {
+      // selected index has changed
+      this.selectedItemIdx -= 1;
+      this.selectedItem = this.masterItems[this.selectedItemIdx];
+    } else if (this.masterItems.length === 0) {
+      // unset select if no elements anymore
+      this.selectedItemIdx = -1;
+      this.selectedItem = undefined;
+    }
+  }
+
+  deleteItem(item: any) {
     this.removeItems(this.propsPath, [item.data])();
   }
 }
