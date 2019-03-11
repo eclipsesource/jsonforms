78cde473a5750cfdd5c109848655bb25fa5a9f5a
Eugen Neufeld <eneufeld@eclipsesource.com>
2019-03-11 18:22:54 +0100

    Added manual tests

--- packages/examples/src/oneOf.ts
+++ packages/examples/src/oneOf.ts
@@ -56,6 +56,114 @@ const data = {
   }
 };
 
+const schema2 = {
+  'type': 'object',
+  'properties': {
+     'coloursOrNumbers': {
+        'oneOf': [
+          //  {
+          //     '$ref': '#/definitions/colours'
+          //  },
+          //  {
+          //     '$ref': '#/definitions/numbers'
+          //  },
+          //  {
+          //     '$ref': '#/definitions/shapes'
+          //  }
+          {
+            '$ref': '#/definitions/foo'
+         },
+         {
+            '$ref': '#/definitions/bar'
+         },
+         {
+            '$ref': '#/definitions/fooBar'
+         }
+        ]
+     }
+  },
+  'definitions': {
+    'foo': {
+        properties: {
+          fooInner: {
+            'title': 'Colours',
+         'type': 'string',
+         'enum': [
+            'Red',
+            'Green',
+            'Blue'
+         ],
+      }
+    }, required: ['fooInner']
+   },
+   'bar': {
+    properties: {
+      barInner: {
+    'title': 'Numbers',
+    'type': 'string',
+    'enum': [
+      'One',
+      'Two',
+      'Three'
+   ],
+  }}, required: ['barInner']
+},
+'fooBar': {
+  properties: {
+    foobarInner: {
+  'title': 'Shapes',
+  'type': 'string',
+  'enum': [
+    'Circle',
+    'Triangle',
+    'Square'
+ ], }}, required: ['foobarInner']
+},
+     'colours': {
+        'title': 'Colours',
+        'type': 'array',
+        'items': {
+           'title': 'Type',
+           'type': 'string',
+           'enum': [
+              'Red',
+              'Green',
+              'Blue'
+           ],
+           'minItems': 1
+        }
+     },
+     'numbers': {
+        'title': 'Numbers',
+        'type': 'array',
+        'items': {
+           'title': 'Type',
+           'type': 'string',
+           'enum': [
+              'One',
+              'Two',
+              'Three'
+           ],
+           'minItems': 1
+        }
+     },
+     'shapes': {
+        'title': 'Shapes',
+        'type': 'array',
+        'items': {
+           'title': 'Type',
+           'type': 'string',
+           'enum': [
+              'Circle',
+              'Triangle',
+              'Square'
+           ],
+           'minItems': 1
+        }
+     }
+  }
+};
+
 registerExamples([
   {
     name: 'oneOf',
@@ -63,5 +171,12 @@ registerExamples([
     data,
     schema,
     uischema
+  },
+  {
+    name: '1265',
+    label: '1265',
+    data: {coloursOrNumbers: {}},
+    schema: schema2,
+    uischema: undefined
   }
 ]);
