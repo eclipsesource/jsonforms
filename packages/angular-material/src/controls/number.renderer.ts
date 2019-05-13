a250aeaeb09b964c42b750184d092bc0320b8425
Eugen Neufeld <eneufeld@eclipsesource.com>
2019-05-13 14:23:27 +0200

    [Angular] Fix review comments

--- packages/angular-material/src/controls/number.renderer.ts
+++ packages/angular-material/src/controls/number.renderer.ts
@@ -56,6 +56,7 @@ import {
   `
 })
 export class NumberControlRenderer extends JsonFormsControl {
+  private readonly MAXIMUM_FRACTIONAL_DIGITS = 20;
   min: number;
   max: number;
   multipleOf: number;
@@ -69,7 +70,19 @@ export class NumberControlRenderer extends JsonFormsControl {
 
   onChange(ev: any) {
     // ignore these
-    if (ev.data === '.' || ev.data === ',' || ev.data === ' ') {
+    if (
+      ev.data === '.' ||
+      ev.data === ',' ||
+      ev.data === ' ' ||
+      // if the value is 0 and we already have a value then we ignore
+      (ev.data === '0' &&
+        this.getValue() !== '' &&
+        // a 0 in the first place
+        ((ev.target.selectionStart === 1 && ev.target.selectionEnd === 1) ||
+          // or in the last place as this doesn't change the value
+          (ev.target.selectionStart === ev.target.value.length &&
+            ev.target.selectionEnd === ev.target.value.length)))
+    ) {
       return;
     }
     super.onChange(ev);
@@ -78,6 +91,10 @@ export class NumberControlRenderer extends JsonFormsControl {
     const cleanPattern = new RegExp(`[^-+0-9${this.decimalSeparator}]`, 'g');
     const cleaned = event.target.value.replace(cleanPattern, '');
     const normalized = cleaned.replace(this.decimalSeparator, '.');
+
+    if (normalized === '') {
+      return undefined;
+    }
 
     // convert to number
     const number = +normalized;
@@ -108,12 +125,16 @@ export class NumberControlRenderer extends JsonFormsControl {
       if (this.locale === undefined || this.locale !== currentLocale) {
         this.locale = currentLocale;
         this.numberFormat = new Intl.NumberFormat(this.locale, {
-          maximumFractionDigits: 20
+          maximumFractionDigits: this.MAXIMUM_FRACTIONAL_DIGITS
         });
-        const example = this.numberFormat.format(1.1);
-        this.decimalSeparator = example.charAt(1);
+        this.determineDecimalSeparator();
       }
     }
+  }
+
+  private determineDecimalSeparator(): void {
+    const example = this.numberFormat.format(1.1);
+    this.decimalSeparator = example.charAt(1);
   }
 }
 export const NumberControlRendererTester: RankedTester = rankWith(
