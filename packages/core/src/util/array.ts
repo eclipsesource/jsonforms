723eb73f36473172a51541fd767286879ecee9e0
Shiv Garg <shivgarg5676@hotmail.com>
2019-06-03 17:24:20 +0530

    array util eslint correction

--- packages/core/src/util/array.ts
+++ packages/core/src/util/array.ts
@@ -1,15 +1,15 @@
-const move = function(array: Array<any>, index: number, delta: number) {
+const move = (array: Array<any>, index: number, delta: number) => {
   const newIndex: number = index + delta;
   if (newIndex < 0 || newIndex >= array.length) return; //Already at the top or bottom.
-  var indexes: number[] = [index, newIndex].sort(); //Sort the indixes
+  const indexes: number[] = [index, newIndex].sort((a, b) => a - b); //Sort the indixes
   array.splice(indexes[0], 2, array[indexes[1]], array[indexes[0]]);
 };
 
-var moveUp = function(array: Array<any>, toMove: number) {
+const moveUp = (array: Array<any>, toMove: number) => {
   move(array, toMove, -1);
 };
 
-var moveDown = function(array: Array<any>, toMove: number) {
+const moveDown = (array: Array<any>, toMove: number) => {
   move(array, toMove, 1);
 };
 
