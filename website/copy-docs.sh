f49c04128fa4ef944f8a7bbd17e5b7bae043cc9f
Stefan Dirix <sdirix@eclipsesource.com>
2019-08-29 11:19:49 +0200

    Update API docs to version 2.3.1

--- website/copy-docs.sh
+++ website/copy-docs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 WEBSITE_REPO=$( dirname "${BASH_SOURCE[0]}" )
 JSONFORMS_REPO=$1
-declare -a PACKAGES=(core react material vanilla angular-material material-tree-renderer ionic)
+declare -a PACKAGES=(core react material vanilla angular angular-material material-tree-renderer ionic)
 
 if [[ -z $JSONFORMS_REPO || ! -d $JSONFORMS_REPO ]]; then
   echo "Error: please specify JSONForms source repo location."
@@ -14,5 +14,8 @@ for package in "${PACKAGES[@]}"; do
   cp -r $JSONFORMS_REPO/packages/$package/docs $WEBSITE_REPO/public/api/$package
   echo "Package $package copied."
 done
+
+# Remove absolute paths (https://github.com/TypeStrong/typedoc/issues/642)
+find $WEBSITE_REPO/public/api -type f -name "*.html" -print0 | xargs -0 sed '/Defined in \/home\//d' -i
 
 exit 0
