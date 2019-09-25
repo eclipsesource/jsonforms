5723922ca04956bedf43c37fb9273e3d29bccac3
Florian Gareis <mail@zoker.me>
2019-09-25 12:54:30 +0200

    Add test for disabled time field

---
+++ packages/material/test/renderers/MaterialNativeControl.test.tsx
@@ -0,0 +1,81 @@
+/*
+  The MIT License
+
+  Copyright (c) 2018-2019 EclipseSource Munich
+  https://github.com/eclipsesource/jsonforms
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+import './MatchMediaMock';
+import React from 'react';
+import Enzyme, { mount, ReactWrapper } from 'enzyme';
+import { MaterialNativeControl } from '../../src/controls/MaterialNativeControl';
+import TextField from '@material-ui/core/TextField';
+import Adapter from 'enzyme-adapter-react-16';
+import { ControlElement, ControlProps } from '@jsonforms/core';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const schema = {
+  type: 'object',
+  properties: {
+    foo: {
+      type: 'string',
+      format: 'time'
+    }
+  }
+};
+const uischema: ControlElement = {
+  type: 'Control',
+  scope: '#/properties/foo'
+};
+
+const createMaterialNativeControl = (props: ControlProps) => {
+  return <MaterialNativeControl {...props} />;
+};
+
+const defaultControlProps = (): ControlProps => {
+  return {
+    handleChange: () => { },
+    enabled: false,
+    visible: true,
+    path: 'path',
+    rootSchema: schema,
+    schema: schema.properties.foo,
+    uischema: uischema,
+    label: 'Foo',
+    id: 'foo-id',
+    errors: '',
+    data: ''
+  };
+};
+
+describe('Material native control', () => {
+  let wrapper: ReactWrapper;
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
+  it('is disabled', () => {
+    const props = defaultControlProps();
+    wrapper = mount(createMaterialNativeControl(props));
+    expect(wrapper.find(TextField).props().disabled).toEqual(true);
+  });
+});
