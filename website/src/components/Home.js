26f3d4300781347191788aac370111d0c4566ed5
Edgar Müller <emueller@eclipsesource.com>
2019-10-09 11:10:47 +0200

    Add twitter widget to landing page at the right (eclipsesource/jsonforms2-website#73)

--- website/src/components/Home.js
+++ website/src/components/Home.js
@@ -7,19 +7,21 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import FeaturesIcon from '@material-ui/icons/Report';
 import CustomizeIcon from '@material-ui/icons/Brush';
 import AddIcon from '@material-ui/icons/Add';
-import {JsonFormsDispatch,JsonFormsReduxContext} from '@jsonforms/react'
+import { JsonFormsDispatch, JsonFormsReduxContext } from '@jsonforms/react'
 import corePackageJson from '@jsonforms/core/package'
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import angularLogo from "../images/angular.svg"
 import reactLogo from '../images/react-logo.svg';
 import reduxLogo from '../images/reduxLogo.svg';
 import schema from './schema.json';
 import uischema from './uischema.json';
 
-import {Demo, Logo} from '../components/common'
-import {createJsonFormsStore} from "../common/store"
+import { Demo, Logo } from '../components/common'
+import { createJsonFormsStore } from "../common/store"
 import globalStyles from '../styles/global.module.css';
 import styles from "./home.module.css"
+import Twitter from "./Twitter";
+import { Grid } from "@material-ui/core";
 //import SEO from "../components/seo"
 
 const additionalStyles = () => ({
@@ -44,6 +46,8 @@ const additionalStyles = () => ({
   }
 });
 
+const JSONFORMS_TWITTER_ID = '3064900589';
+
 const store = createJsonFormsStore({
   data: { firstName: 'Max Power' },
   schema,
@@ -55,37 +59,43 @@ const Home = ({ classes }) => {
     <React.Fragment>
       {/*<SEO title="Home" keywords={[`jsonschema`, `json`, `react`, `forms`]} />*/}
       <div className={classes.logo}>
-        <Typography variant='h2'>
-          <strong>JSON</strong>Forms
-        </Typography>
         <div style={{ paddingTop: '0.5em' }}>
-          <Logo />
+          <Grid container>
+            <Grid item sm={4} md={4}></Grid>
+            <Grid item xs={12} sm={12} md={4} justify='center'>
+              <Typography variant='h2'>
+                <strong>JSON</strong>Forms
+              </Typography>
+              <Logo />
+              <Typography variant='h5'>
+                More forms. Less code.
+              </Typography>
+              <div className={styles.feature}>
+                <Typography variant='h6' style={{ color: '#747474' }}>
+                  Complex forms in the blink of an eye
+                </Typography>
+              </div>
+              <div className={styles.feature}>
+                <Typography>
+                  Version {corePackageJson.version}
+                </Typography>
+              </div>
+              <div className={styles.feature}>
+                <Link
+                  to='/docs/getting-started'
+                  className={globalStyles.link}
+                >
+                  <Button variant='outlined' className={classes.gettingStartedButton}>
+                    Get started
+                  </Button>
+                </Link>
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={12} md={4} justify='flex-start'>
+              <Twitter id={JSONFORMS_TWITTER_ID} width={400} height={400} />
+            </Grid>
+          </Grid>
         </div>
-        <Typography variant='h5'>
-          More forms. Less code.
-        </Typography>
-      </div>
-
-      <div className={styles.feature}>
-        <Typography variant='h6' style={{ color: '#747474' }}>
-          Complex forms in the blink of an eye
-        </Typography>
-      </div>
-      <div className={styles.feature}>
-        <Typography>
-          Version {corePackageJson.version}
-        </Typography>
-      </div>
-
-      <div className={styles.feature}>
-        <Link
-          to='/docs/getting-started'
-          className={globalStyles.link}
-        >
-          <Button variant='outlined' className={classes.gettingStartedButton}>
-            Get started
-          </Button>
-        </Link>
       </div>
 
       <div className={styles.landing_page__features}>
@@ -127,16 +137,16 @@ const Home = ({ classes }) => {
 
       <div className={styles.landing_page__form}>
         <Provider store={store}>
-         <JsonFormsReduxContext>
+          <JsonFormsReduxContext>
             <Demo
               js={() => <JsonFormsDispatch />}
               schema={schema}
               uischema={uischema}
             />
-            </JsonFormsReduxContext>
+          </JsonFormsReduxContext>
         </Provider>
       </div>
-    </React.Fragment>
+    </React.Fragment >
   );
 };
 
