1b0ba7b6bebc15a1991d837bb3c2537784b8bbef
Florian Gareis <mail@zoker.me>
2019-12-19 13:47:02 +0100

    Remove not used state

--- website/src/components/DocPage.js
+++ website/src/components/DocPage.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import { useMenus, Link } from "docz";
 import withStyles from "@material-ui/core/styles/withStyles";
 import SidebarLayout from "./common/sidebar/SidebarLayout";
@@ -6,14 +6,12 @@ import { Button } from "@material-ui/core";
 import styles from "../styles/global.module.css";
 
 const DocPage = ({ children, pathname }) => {
-  const [query, setQuery] = useState('')
-  const filteredMenus = useMenus({ query });
-  const menus = useMenus({ 
-    filter: m => m.route.startsWith('/docs/') && (query ? true : !m.parent),
+  const menus = useMenus({
+    filter: m => m.route.startsWith('/docs/') && !m.parent,
   });
   return (
     <Fragment>
-      <SidebarLayout menus={query ? filteredMenus : menus} pathname={pathname}>
+      <SidebarLayout menus={menus} pathname={pathname}>
         {children}
         <Button variant="outlined" component={Link} to='/docs' className={styles.toc_button}>
           Back to Overview
