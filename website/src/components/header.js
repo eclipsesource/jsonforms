2eb9737e8a8afdadfdd583b7dbac87ca731976c6
Florian Gareis <mail@zoker.me>
2019-12-19 13:45:53 +0100

    Set new default menu item links

--- website/src/components/header.js
+++ website/src/components/header.js
@@ -45,10 +45,10 @@ const Header = ({ classes }) => (
           JSON Forms
         </Typography>
       </Link>
-      <Link to="/examples" className={globalStyles.nav__link}>
+      <Link to="/examples/array" className={globalStyles.nav__link}>
         Examples
       </Link>
-      <Link to="/docs" className={globalStyles.nav__link}>
+      <Link to="/docs/what-is-jsonforms" className={globalStyles.nav__link}>
         Docs
       </Link>
       <Link to="/faq" className={globalStyles.nav__link}>
