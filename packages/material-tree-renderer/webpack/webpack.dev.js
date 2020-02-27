5c6b3d545e337a739d7b470d7d8ad6bdb8ef1906
Stefan Dirix <sdirix@eclipsesource.com>
2020-02-27 11:35:38 +0100

    Extract material tree renderer into its own repository

--- packages/material-tree-renderer/webpack/webpack.dev.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge');
-const baseConfig = require('../../../webpack/webpack.dev.base.js');
-
-module.exports = merge(baseConfig, {
-  devServer: {
-    contentBase: ['./src','./example/icons', './example'],
-    port: 8080
-  }
-});
