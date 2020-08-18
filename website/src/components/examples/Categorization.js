4c51ea25cac19843429f2895254047d841f83d9a
Florian Gareis <TheZoker@users.noreply.github.com>
2020-08-18 16:53:57 +0200

    Add stepper example to categorization page

--- website/src/components/examples/Categorization.js
+++ website/src/components/examples/Categorization.js
@@ -1,33 +1,60 @@
 import React from 'react';
-import { categorization } from '@jsonforms/examples';
+import { categorization, stepper, steppershownav } from '@jsonforms/examples';
 import { JsonFormsDispatch, JsonFormsReduxContext } from '@jsonforms/react';
 import { Provider } from 'react-redux';
 import Demo from '../common/Demo';
 import { createJsonFormsStore } from '../../common/store';
 
-const CategorizationExample = () => {
-  const store = createJsonFormsStore({
-    data: {
-      name: 'Max Power',
-      vegetarian: true,
-      birthDate: '1956-05-12',
-      nationality: 'US'
-    },
-    schema: categorization.schema,
-    uischema: categorization.uischema
-  });
+const categorizationStore = createJsonFormsStore({
+  data: categorization.data,
+  schema: categorization.schema,
+  uischema: categorization.uischema
+});
 
-  return (
-    <Provider store={store}>
-      <JsonFormsReduxContext>
-        <Demo
-          schema={categorization.schema}
-          uischema={categorization.uischema}
-          js={() => <JsonFormsDispatch />}
-        />
-      </JsonFormsReduxContext>
-    </Provider>
-  );
-};
+const categorizationStepperStore = createJsonFormsStore({
+  data: stepper.data,
+  schema: stepper.schema,
+  uischema: stepper.uischema
+});
 
-export default CategorizationExample;
+const categorizationStepperNavStore = createJsonFormsStore({
+  data: steppershownav.data,
+  schema: steppershownav.schema,
+  uischema: steppershownav.uischema
+});
+
+export const Categorization = () => (
+  <Provider store={categorizationStore}>
+    <JsonFormsReduxContext>
+      <Demo
+        js={() => <JsonFormsDispatch />}
+        schema={categorization.schema}
+        uischema={categorization.uischema}
+      />
+    </JsonFormsReduxContext>
+  </Provider>
+);
+
+export const CategorizationStepper = () => (
+  <Provider store={categorizationStepperStore}>
+    <JsonFormsReduxContext>
+      <Demo
+        js={() => <JsonFormsDispatch />}
+        schema={stepper.schema}
+        uischema={stepper.uischema}
+      />
+    </JsonFormsReduxContext>
+  </Provider>
+);
+
+export const CategorizationStepperNav = () => (
+  <Provider store={categorizationStepperNavStore}>
+    <JsonFormsReduxContext>
+      <Demo
+        js={() => <JsonFormsDispatch />}
+        schema={steppershownav.schema}
+        uischema={steppershownav.uischema}
+      />
+    </JsonFormsReduxContext>
+  </Provider>
+);
