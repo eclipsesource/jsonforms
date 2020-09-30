cc0c7e2e402cf9904ef5f339097be4908cfc0589
Florian Gareis <TheZoker@users.noreply.github.com>
2020-09-30 21:28:06 +0200

    Add "Control" example
    Adds a new "Control" example showcasing general control options.

---
+++ website/src/components/examples/Control.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { controlOptions } from '@jsonforms/examples';
+import { JsonFormsDispatch, JsonFormsReduxContext } from '@jsonforms/react';
+import { Provider } from 'react-redux';
+import { createJsonFormsStore } from '../../common/store';
+import Demo from '../common/Demo';
+
+const controlStore = createJsonFormsStore({
+  data: controlOptions.data,
+  schema: controlOptions.schema,
+  uischema: controlOptions.uischema
+});
+
+const extendedControlStore = createJsonFormsStore({
+  data: controlOptions.extendedData,
+  schema: controlOptions.extendedSchema,
+  uischema: controlOptions.extendedUischema
+});
+
+export const Control = () => (
+  <Provider store={controlStore}>
+    <JsonFormsReduxContext>
+      <Demo
+        js={() => <JsonFormsDispatch />}
+        schema={controlOptions.schema}
+        uischema={controlOptions.uischema}
+      />
+    </JsonFormsReduxContext>
+  </Provider>
+);
+
+export const ExtendedControl = () => (
+  <Provider store={extendedControlStore}>
+    <JsonFormsReduxContext>
+      <Demo
+        js={() => <JsonFormsDispatch />}
+        schema={controlOptions.extendedSchema}
+        uischema={controlOptions.extendedUischema}
+      />
+    </JsonFormsReduxContext>
+  </Provider>
+);
+
