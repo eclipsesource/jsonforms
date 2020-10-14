df2cb8b026bd5baeefbf03ca17b03994bfa00edc
Florian Gareis <mail@zoker.me>
2020-10-14 17:19:45 +0200

    Add missing copyright and fix eslint warnings

--- packages/core/src/util/type.ts
+++ packages/core/src/util/type.ts
@@ -1,5 +1,30 @@
+/*
+  The MIT License
+
+  Copyright (c) 2017-2019 EclipseSource Munich
+  https://github.com/eclipsesource/jsonforms
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+
 export interface Action<T = any> {
-  type: T
+  type: T;
 }
 
 /**
@@ -10,7 +35,7 @@ export interface Action<T = any> {
  */
 export interface AnyAction extends Action {
   // Allows any extra properties to be defined in an action.
-  [extraProps: string]: any
+  [extraProps: string]: any;
 }
 
 /**
@@ -34,9 +59,7 @@ export interface AnyAction extends Action {
  * @template A The type of things (actions or otherwise) which may be
  *   dispatched.
  */
-export interface Dispatch<A extends Action = AnyAction> {
-  <T extends A>(action: T): T
-}
+export type Dispatch<A extends Action = AnyAction> = <T extends A>(action: T) => T;
 
 /**
  * A store is an object that holds the application's state tree.
@@ -73,14 +96,14 @@ export interface Store<S = any, A extends Action = AnyAction> {
    * Note that, if you use a custom middleware, it may wrap `dispatch()` to
    * return something else (for example, a Promise you can await).
    */
-  dispatch: Dispatch<A>
+  dispatch: Dispatch<A>;
 
   /**
    * Reads the state tree managed by the store.
    *
    * @returns The current state tree of your application.
    */
-  getState(): S
+  getState(): S;
 
   /**
    * Interoperability point for observable/reactive libraries.
@@ -88,40 +111,13 @@ export interface Store<S = any, A extends Action = AnyAction> {
    * For more information, see the observable proposal:
    * https://github.com/tc39/proposal-observable
    */
-  [Symbol.observable](): Observable<S>
+  [Symbol.observable](): Observable<S>;
 }
 
 /**
  * Function to remove listener added by `Store.subscribe()`.
  */
-export interface Unsubscribe {
-  (): void
-}
-
-/**
- * A *dispatching function* (or simply *dispatch function*) is a function that
- * accepts an action or an async action; it then may or may not dispatch one
- * or more actions to the store.
- *
- * We must distinguish between dispatching functions in general and the base
- * `dispatch` function provided by the store instance without any middleware.
- *
- * The base dispatch function *always* synchronously sends an action to the
- * store's reducer, along with the previous state returned by the store, to
- * calculate a new state. It expects actions to be plain objects ready to be
- * consumed by the reducer.
- *
- * Middleware wraps the base dispatch function. It allows the dispatch
- * function to handle async actions in addition to actions. Middleware may
- * transform, delay, ignore, or otherwise interpret actions or async actions
- * before passing them to the next middleware.
- *
- * @template A The type of things (actions or otherwise) which may be
- *   dispatched.
- */
-export interface Dispatch<A extends Action = AnyAction> {
-  <T extends A>(action: T): T
-}
+export type Unsubscribe = () => void;
 
 /**
  * A minimal observable of state changes.
@@ -137,9 +133,9 @@ export type Observable<T> = {
    * be used to unsubscribe the observable from the store, and prevent further
    * emission of values from the observable.
    */
-  subscribe: (observer: Observer<T>) => { unsubscribe: Unsubscribe }
+  subscribe(observer: Observer<T>): { unsubscribe: Unsubscribe };
   [Symbol.observable](): Observable<T>
-}
+};
 
 /**
  * An Observer is used to receive data from an Observable, and is supplied as
@@ -147,4 +143,4 @@ export type Observable<T> = {
  */
 export type Observer<T> = {
   next?(value: T): void
-}
+};
