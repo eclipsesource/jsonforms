5379eafbfe828f6fd896c819a1c2f65b291ade73
Florian Gareis <mail@zoker.me>
2020-10-21 20:49:46 +0200

    Add missing functions, remove unnecessary and add copy notice to type.ts

--- packages/core/src/util/type.ts
+++ packages/core/src/util/type.ts
@@ -23,10 +23,12 @@
   THE SOFTWARE.
 */
 
+// Copied from https://github.com/reduxjs/redux/blob/master/src/types/actions.ts
 export interface Action<T = any> {
   type: T;
 }
 
+// Copied from https://github.com/reduxjs/redux/blob/master/src/types/actions.ts
 /**
  * An Action type which accepts any other properties.
  * This is mainly for the use of the `Reducer` type.
@@ -38,6 +40,7 @@ export interface AnyAction extends Action {
   [extraProps: string]: any;
 }
 
+// Copied from https://github.com/reduxjs/redux/blob/master/src/types/store.ts
 /**
  * A *dispatching function* (or simply *dispatch function*) is a function that
  * accepts an action or an async action; it then may or may not dispatch one
@@ -61,6 +64,7 @@ export interface AnyAction extends Action {
  */
 export type Dispatch<A extends Action = AnyAction> = <T extends A>(action: T) => T;
 
+// Copied from https://github.com/reduxjs/redux/blob/master/src/types/store.ts
 /**
  * A store is an object that holds the application's state tree.
  * There should only be a single store in a Redux app, as the composition
@@ -106,41 +110,75 @@ export interface Store<S = any, A extends Action = AnyAction> {
   getState(): S;
 
   /**
-   * Interoperability point for observable/reactive libraries.
-   * @returns {observable} A minimal observable of state changes.
-   * For more information, see the observable proposal:
-   * https://github.com/tc39/proposal-observable
+   * Adds a change listener. It will be called any time an action is
+   * dispatched, and some part of the state tree may potentially have changed.
+   * You may then call `getState()` to read the current state tree inside the
+   * callback.
+   *
+   * You may call `dispatch()` from a change listener, with the following
+   * caveats:
+   *
+   * 1. The subscriptions are snapshotted just before every `dispatch()` call.
+   * If you subscribe or unsubscribe while the listeners are being invoked,
+   * this will not have any effect on the `dispatch()` that is currently in
+   * progress. However, the next `dispatch()` call, whether nested or not,
+   * will use a more recent snapshot of the subscription list.
+   *
+   * 2. The listener should not expect to see all states changes, as the state
+   * might have been updated multiple times during a nested `dispatch()` before
+   * the listener is called. It is, however, guaranteed that all subscribers
+   * registered before the `dispatch()` started will be called with the latest
+   * state by the time it exits.
+   *
+   * @param listener A callback to be invoked on every dispatch.
+   * @returns A function to remove this change listener.
    */
-  [Symbol.observable](): Observable<S>;
+  subscribe(listener: () => void): Unsubscribe;
+
+  /**
+   * Replaces the reducer currently used by the store to calculate the state.
+   *
+   * You might need this if your app implements code splitting and you want to
+   * load some of the reducers dynamically. You might also need this if you
+   * implement a hot reloading mechanism for Redux.
+   *
+   * @param nextReducer The reducer for the store to use instead.
+   */
+  replaceReducer(nextReducer: Reducer<S, A>): void;
 }
 
+// Copied from https://github.com/reduxjs/redux/blob/master/src/types/reducers.ts
+/**
+ * A *reducer* (also called a *reducing function*) is a function that accepts
+ * an accumulation and a value and returns a new accumulation. They are used
+ * to reduce a collection of values down to a single value
+ *
+ * Reducers are not unique to Redux—they are a fundamental concept in
+ * functional programming.  Even most non-functional languages, like
+ * JavaScript, have a built-in API for reducing. In JavaScript, it's
+ * `Array.prototype.reduce()`.
+ *
+ * In Redux, the accumulated value is the state object, and the values being
+ * accumulated are actions. Reducers calculate a new state given the previous
+ * state and an action. They must be *pure functions*—functions that return
+ * the exact same output for given inputs. They should also be free of
+ * side-effects. This is what enables exciting features like hot reloading and
+ * time travel.
+ *
+ * Reducers are the most important concept in Redux.
+ *
+ * *Do not put API calls into reducers.*
+ *
+ * @template S The type of state consumed and produced by this reducer.
+ * @template A The type of actions the reducer can potentially respond to.
+ */
+export type Reducer<S = any, A extends Action = AnyAction> = (
+  state: S | undefined,
+  action: A
+) => S;
+
+// Copied from https://github.com/reduxjs/redux/blob/master/src/types/store.ts
 /**
  * Function to remove listener added by `Store.subscribe()`.
  */
 export type Unsubscribe = () => void;
-
-/**
- * A minimal observable of state changes.
- * For more information, see the observable proposal:
- * https://github.com/tc39/proposal-observable
- */
-export type Observable<T> = {
-  /**
-   * The minimal observable subscription method.
-   * @param {Object} observer Any object that can be used as an observer.
-   * The observer object should have a `next` method.
-   * @returns {subscription} An object with an `unsubscribe` method that can
-   * be used to unsubscribe the observable from the store, and prevent further
-   * emission of values from the observable.
-   */
-  subscribe(observer: Observer<T>): { unsubscribe: Unsubscribe };
-  [Symbol.observable](): Observable<T>
-};
-
-/**
- * An Observer is used to receive data from an Observable, and is supplied as
- * an argument to subscribe.
- */
-export type Observer<T> = {
-  next?(value: T): void
-};
