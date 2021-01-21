24762b1067c31055297092b042675349e29b89fb
buryndin <buryndin@yandex.ru>
2021-01-21 12:55:09 +0700

    Added auto anyOf/oneOf/allOf resolving

--- packages/core/src/util/resolvers.ts
+++ packages/core/src/util/resolvers.ts
@@ -1,19 +1,19 @@
 /*
   The MIT License
-  
+
   Copyright (c) 2017-2019 EclipseSource Munich
   https://github.com/eclipsesource/jsonforms
-  
+
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
   in the Software without restriction, including without limitation the rights
   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
   copies of the Software, and to permit persons to whom the Software is
   furnished to do so, subject to the following conditions:
-  
+
   The above copyright notice and this permission notice shall be included in
   all copies or substantial portions of the Software.
-  
+
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -125,15 +125,29 @@ export const resolveSchema = (
   const validPathSegments = schemaPath.split('/');
   const invalidSegment = (pathSegment: string) =>
     pathSegment === '#' || pathSegment === undefined || pathSegment === '';
-  const resultSchema = validPathSegments.reduce((curSchema, pathSegment) => {
-    curSchema =
-      curSchema === undefined || curSchema.$ref === undefined
-        ? curSchema
-        : resolveSchema(schema, curSchema.$ref);
-    return invalidSegment(pathSegment)
-      ? curSchema
-      : get(curSchema, pathSegment);
-  }, schema);
+  let resultSchema = schema;
+  for (let i = 0; i < validPathSegments.length; i++) {
+    let pathSegment = validPathSegments[i];
+    resultSchema =
+      resultSchema === undefined || resultSchema.$ref === undefined
+        ? resultSchema
+        : resolveSchema(schema, resultSchema.$ref);
+    if (invalidSegment(pathSegment)) {
+      continue;
+    }
+    let curSchema = get(resultSchema, pathSegment);
+    if (curSchema) {
+      resultSchema = curSchema;
+      continue;
+    }
+    let schemas = resultSchema.oneOf || resultSchema.allOf || resultSchema.anyOf || [];
+    for (let item of schemas) {
+      curSchema = resolveSchema(item, validPathSegments.slice(i).join('/'));
+      if (curSchema) break;
+    }
+    resultSchema = curSchema;
+    break;
+  }
   // TODO: because schema is already scoped we might end up with refs pointing
   // outside of the current schema. It would be better if we'd always could deal
   // with absolute paths here, so that we don't need to keep two different
