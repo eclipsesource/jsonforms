075bbc448cfb2bb209e86d6b1ea6460df23a9f64
Stefan Dirix <sdirix@eclipsesource.com>
2021-12-22 22:28:57 +0100

    Fix React Vanilla build
    Lerna removed the empty dependency attribute of the React Vanilla
    package.json during the release. The React Vanilla rollup build
    tried to access this attribute and therefore crashed. This is now
    fixed.

--- packages/vanilla/rollup.config.js
+++ packages/vanilla/rollup.config.js
@@ -7,7 +7,7 @@ const packageJson = require('./package.json');
 const baseConfig = {
   input: 'src/index.ts',
   external: [
-    ...Object.keys(packageJson.dependencies),
+    ...Object.keys(packageJson.dependencies ?? {}),
     ...Object.keys(packageJson.peerDependencies),
     'react',
     /^lodash\/.*/,
