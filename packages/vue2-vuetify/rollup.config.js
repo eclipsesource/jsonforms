0aae0e98d1976281be34c8525b582c593d8d7c1e
Stefan Dirix <sdirix@eclipsesource.com>
2022-01-10 18:01:06 +0100

    Build in CommonJS and ESM formats
    JSON Forms Vue 2 Vuetitfy is now built in CommonJS and ESM format,
    similar to the core repository.

--- packages/vue2-vuetify/rollup.config.js
+++ packages/vue2-vuetify/rollup.config.js
@@ -2,48 +2,35 @@ import vue from 'rollup-plugin-vue';
 import babel from '@rollup/plugin-babel';
 import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import cleanup from 'rollup-plugin-cleanup';
 import visualizer from 'rollup-plugin-visualizer';
 
 import packageJson from './package.json';
 
+const baseConfig = {
+  input: 'src/index.ts',
+  external: [
+    ...Object.keys(packageJson.dependencies),
+    ...Object.keys(packageJson.peerDependencies),
+    /^lodash\/.*/,
+    'vuetify/lib',
+    '@mdi/font',
+    /^dayjs\/.*/,
+  ],
+};
+
 const buildFormats = [
   {
-    input: 'src/index.ts',
+    ...baseConfig,
     output: {
-      file: packageJson.main,
-      format: 'cjs',
-      exports: 'named',
+      file: packageJson.module,
+      format: 'esm',
       sourcemap: true,
     },
-    external: [
-      'vue',
-      '@vue/composition-api',
-      '@jsonforms/core',
-      '@jsonforms/vue2',
-      'lodash',
-      'lodash/startCase',
-      'lodash/isEmpty',
-      'lodash/findIndex',
-      'lodash/merge',
-      'lodash/cloneDeep',
-      'lodash/mergeWith',
-      'lodash/isArray',
-      'lodash/every',
-      'lodash/isString',
-      'lodash/omit',
-      'vuetify/lib',
-      '@mdi/font',
-      'dayjs',
-      'dayjs/plugin/customParseFormat',
-      'dayjs/plugin/utc',
-      'dayjs/plugin/timezone',
-    ],
     plugins: [
       resolve({
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
       }),
-      commonjs(),
       vue({
         css: true,
         template: {
@@ -55,13 +42,51 @@ const buildFormats = [
       }),
       typescript({
         emitDeclarationOnly: true,
+        tsconfigOverride: {
+          target: 'ES5',
+        },
       }),
       babel({
         exclude: 'node_modules/**',
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
         babelHelpers: 'bundled',
       }),
+      cleanup({ extensions: ['js', 'ts', 'jsx', 'tsx', 'vue'] }),
       visualizer(),
+    ],
+  },
+  {
+    ...baseConfig,
+    output: {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    plugins: [
+      resolve({
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+      }),
+      vue({
+        css: true,
+        template: {
+          isProduction: true,
+        },
+        // rollup-plugin-vue can't handle Vue source maps in watch mode
+        // https://github.com/vuejs/rollup-plugin-vue/issues/238
+        needMap: !process.env.ROLLUP_WATCH,
+      }),
+      typescript({
+        emitDeclarationOnly: true,
+        tsconfigOverride: {
+          target: 'ES5',
+        },
+      }),
+      babel({
+        exclude: 'node_modules/**',
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+        babelHelpers: 'bundled',
+      }),
+      cleanup({ extensions: ['js', 'ts', 'jsx', 'tsx', 'vue'] }),
     ],
   },
 ];
