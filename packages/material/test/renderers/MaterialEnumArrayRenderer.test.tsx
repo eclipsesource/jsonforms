d04e851f56537bcf6bda9240ab7e0ae5f09b4010
JoshuaFortriede <51160626+JoshuaFortriede@users.noreply.github.com>
2022-05-05 09:13:20 -0400

    Don't change enum labels in React Material enum arrays
    Remove the 'startCase' invocation on React Material enum arrays. Enums will
    now be displayed as they are defined.
    
    Co-authored-by: Florian Gareis <dev@zoker.me>

--- packages/material/test/renderers/MaterialEnumArrayRenderer.test.tsx
+++ packages/material/test/renderers/MaterialEnumArrayRenderer.test.tsx
@@ -113,7 +113,7 @@ describe('EnumArrayControl', () => {
     );
     const labels = wrapper.find('label');
     expect(labels.first().text()).toBe('My Title');
-    expect(labels.last().text()).toBe('Bar');
+    expect(labels.last().text()).toBe('bar');
   });
 
   test('oneOf items - updates data', (done) => {
@@ -176,9 +176,9 @@ describe('EnumArrayControl', () => {
       />
     );
     const labels = wrapper.find('label');
-    expect(labels.at(0).text()).toBe('A');
-    expect(labels.at(1).text()).toBe('B');
-    expect(labels.at(2).text()).toBe('C');
+    expect(labels.at(0).text()).toBe('a');
+    expect(labels.at(1).text()).toBe('b');
+    expect(labels.at(2).text()).toBe('c');
   });
 
   test('enum items - updates data', (done) => {
