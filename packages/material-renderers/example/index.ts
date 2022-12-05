96be5205ba37203b25485e274b3ca94d30f8616d
Lucas Koehler <lkoehler@eclipsesource.com>
2022-12-05 12:08:55 +0100

    Adapt package folder names to match package names
    - Rename package folder `example` to `examples-react`
    - Rename package folder `material` to `material-renderers`
    - Rename package folder `vanilla` to `vanilla-renderers`
    
    Resolves #1036

--- packages/material-renderers/example/index.ts
+++ packages/material-renderers/example/index.ts
@@ -22,7 +22,7 @@
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
   THE SOFTWARE.
 */
-import { renderExample } from '../../example/src/index';
+import { renderExample } from '../../examples-react/src/index';
 import { materialRenderers, materialCells } from '../src';
 
 renderExample(
