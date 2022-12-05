456f3f2e732cd972b0519d8b33aa47de8fea7a71
Lucas Koehler <lkoehler@eclipsesource.com>
2022-12-05 14:41:19 +0100

    Add examples app with bundled react vanilla, material examples
    This introduces a new private package `examples-app` to create a simple web page
    that ultimately has child pages for every example.
    
    In this initial commit, only the react vanilla and material examples are added.
    Therefore, they are bundled with rollup to be able to run as standalone pages.
    
    - Add rollup configs to bundle react vanilla and material examples
    - Add required dev deps and upgrade rollup dev dep to ^2.78.0 for all packages
    - change material deps to ~5.2.2 because latest 5.x versions break api for Autocomplete
    - Add private package `examples-app` with a script to aggregate the examples
    - Add root level npm script to create the examples app
    - regenerate package-lock.json
    
    Part of #1706

---
+++ packages/examples-app/README.md
@@ -0,0 +1,12 @@
+# JSON Forms - More Forms. Less Code
+
+*Complex forms in the blink of an eye*
+
+JSON Forms eliminates the tedious task of writing fully-featured forms by hand by leveraging the capabilities of JSON, JSON Schema and Javascript.
+
+## Examples App
+
+This package aggregates JSON Forms examples and makes them reachable via single `index.html` file.
+
+Aggregating the examples assumes that packages were built and their e examples bundled.
+Afterwards, the examples app can be aggregated with [prepare-examples-app.js](./prepare-examples-app.js) into folder `dist`.
---
+++ packages/examples-app/prepare-examples-app.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import { copySync } from 'fs-extra/esm';
+import { copyFileSync, mkdirSync, rmdirSync } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const distDir = join(__dirname, 'dist');
+const packagesDir = join(__dirname, '..');
+const examples = {
+  'react-vanilla': join(packagesDir, 'vanilla', 'example', 'dist'),
+  'react-material': join(packagesDir, 'material', 'example', 'dist')
+}
+
+// Clean and recreate dist dir
+console.log('Clean and recreate dist dir...');
+rmdirSync(distDir, { recursive: true, force: true });
+mkdirSync(distDir, { recursive: true });
+
+// Copy index and built examples
+console.log('Copy index.html...');
+console.log('Copy example apps...');
+copyFileSync(join(__dirname, 'index.html'), join(distDir, 'index.html'));
+Object.keys(examples).forEach(key => {
+  console.log(`Copying example ${key}...`);
+  const path = examples[key];
+  copySync(path, join(distDir, key));
+});
+
+console.log('...finished');
