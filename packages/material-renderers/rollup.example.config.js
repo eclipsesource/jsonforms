5a84cd3ba806cb3febd1687b5ace4b1a4e436fe1
Lucas Koehler <lkoehler@eclipsesource.com>
2023-01-04 17:15:39 +0100

    Add examples app with React Vanilla and Material UI renderers
    This introduces a new private package `examples-app` to create a simple web page
    that ultimately has child pages for every example.
    
    In this initial commit, only the react vanilla and material renderers are added.
    Therefore, they are bundled with rollup to be able to run as standalone pages.
    
    - Add rollup configs to bundle react vanilla and material examples
    - Add required dev deps and upgrade rollup dev dep to ^2.78.0 for all packages
    - change material deps to ~5.2.2 because latest 5.x versions break api for Autocomplete
    - Add private package `examples-app` with a script to aggregate the examples
    - Add root level npm script to create the examples app
    - regenerate package-lock.json
    
    Part of #1706

---
+++ packages/material-renderers/rollup.example.config.js
@@ -0,0 +1,52 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import copy from 'rollup-plugin-copy';
+import css from 'rollup-plugin-import-css';
+import typescript from 'rollup-plugin-typescript2';
+
+/**
+ * @type {import('rollup').RollupOptions}
+*/
+const config = {
+  input: 'example/index.ts',
+  output: {
+    file: 'example/dist/bundle.js',
+    format: 'iife',
+    sourcemap: true
+  },
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      preventAssignment: true, // recommended to be set by library to be forward compatible
+    }),
+    nodeResolve({ browser: true }),
+    // Transform mixed because some JsonForms modules use import and require
+    commonjs({ transformMixedEsModules: true }),
+    css(),
+    json(),
+    typescript({
+      tsconfigOverride: {
+        compilerOptions: {
+          // Do not emit typescript declarations for our bundled example app
+          declaration: false
+        }
+    }}),
+    copy({
+      targets: [
+        {
+          src: 'example/index.bundled.html',
+          dest: 'example/dist',
+          rename: () => 'index.html'
+        },
+        {
+          src: '../examples-react/src/logo.svg',
+          dest: 'example/dist/assets'
+        }
+      ]
+    }),
+  ]
+}
+
+export default config;
