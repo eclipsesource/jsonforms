39b4dc682abbbd19581e4b6273d886a89cdf876a
Lucas Koehler <lkoehler@eclipsesource.com>
2023-04-14 12:48:42 +0200

    Replace TSLint with ESLint and Prettier configuration
    - Remove TSLint dependencies and config
    - Add ESLint and Prettier dependencies and config for all packages
    - Add markdownlint to recommended extensions
    - Use ESLint 7.x instead of 8.x because the Angular eslint plugin
      for Angular 12 does not work with ESLint 8.x
    
    Disabled rules for to be re-enabled later:
    - explicit any warnings
    - no-prototype-builtins rule
    - import/no-named-as-default for 2 packages
      - vanilla-renderers
      - material-renderers
    
    Part of #1551

---
+++ packages/core/.eslintrc.js
@@ -0,0 +1,36 @@
+/* eslint-env node */
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  // There is no file include in ESLint. Thus, ignore all and include files via negative ignore (!)
+  ignorePatterns: ['/*', '!/src', '!/test'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    // Allow extending interfaces without additional properties to create marker interfaces
+    '@typescript-eslint/no-empty-interface': [
+      'error',
+      {
+        allowSingleExtends: true,
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-prototype-builtins': 'off',
+    // Base rule must be disabled to avoid incorrect errors
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+  },
+};
