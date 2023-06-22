184879a5bf0e566b16c0f3e60b5b1907b6d4783e
Stefan Dirix <sdirix@eclipsesource.com>
2023-06-22 17:01:22 +0200

    chore: fix build by reducing dependencies

--- website/babel.config.js
+++ website/babel.config.js
@@ -1,8 +1,3 @@
 module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
-  plugins: [
-    "@babel/plugin-proposal-class-properties",
-    ["@babel/plugin-proposal-private-methods", { "loose": false }],
-    ["@babel/plugin-proposal-private-property-in-object", { "loose": false }]
-  ]
 };
