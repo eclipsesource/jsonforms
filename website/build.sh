6c0f5dab6ea1158bcffcaab83a91e9f83ba132d4
Florian Gareis <TheZoker@users.noreply.github.com>
2023-09-15 17:40:42 +0200

    Add default security headers
    Adds security headers. They need to be adapted whenever the Cloudflare beacon changes.

--- website/build.sh
+++ website/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# 
+# Add versions to frontpage
+#
+
 function next() {
     NEXTVERSIONCANDIDATE=$(curl --silent "https://api.github.com/repos/eclipsesource/jsonforms/tags" | grep '"name":' | head -1 | sed -E 's/.*"([^"]+)".*/\1/')
     if [[ ${NEXTVERSIONCANDIDATE:0:1} == "v" ]] && [[ $NEXTVERSIONCANDIDATE != $CURRENTVERSION ]] ; then NEXTVERSION="${NEXTVERSIONCANDIDATE:1}"; fi
