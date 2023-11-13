1a7cdda62f175b994cff754f1f65047424b9098f
JBBianchi <jb.bianchi@neuroglia.io>
2023-11-13 18:23:21 +0100

    angular-material: (???) fixed karma test shim

--- packages/angular-material/test-config/karma-test-shim.js
+++ packages/angular-material/test-config/karma-test-shim.js
@@ -1,15 +1,16 @@
 Error.stackTraceLimit = Infinity;
 
-require('core-js/es6');
-require('core-js/es7/reflect');
+// require('core-js/es6');
+// require('core-js/es7/reflect');
 
-require('zone.js/dist/zone');
-require('zone.js/dist/long-stack-trace-zone');
-require('zone.js/dist/proxy');
-require('zone.js/dist/sync-test');
-require('zone.js/dist/jasmine-patch');
-require('zone.js/dist/async-test');
-require('zone.js/dist/fake-async-test');
+// require('zone.js/dist/zone');
+// require('zone.js/dist/long-stack-trace-zone');
+// require('zone.js/dist/proxy');
+// require('zone.js/dist/sync-test');
+// require('zone.js/dist/jasmine-patch');
+// require('zone.js/dist/async-test');
+// require('zone.js/dist/fake-async-test');
+require('zone.js');
 require('zone.js/testing');
 
 var appContext = require.context('../test', true, /\.spec\.ts/);
