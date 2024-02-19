f24ad1d6ff772980aa1aa57b4934ad04c519bac4
David Wolter <david@dampfer.net>
2024-02-19 18:01:22 +0100

    move categorization renderers to layouts directory

--- packages/vue-vanilla/src/complex/index.ts
+++ packages/vue-vanilla/src/complex/index.ts
@@ -1,19 +1,13 @@
 export { default as ObjectRenderer } from './ObjectRenderer.vue';
 export { default as OneOfRenderer } from './OneOfRenderer.vue';
 export { default as EnumArrayRenderer } from './EnumArrayRenderer.vue';
-export { default as CategorizationRenderer } from './CategorizationRenderer.vue';
-export { default as CategorizationStepperRenderer } from './CategorizationStepperRenderer.vue';
 
 import { entry as objectRendererEntry } from './ObjectRenderer.vue';
 import { entry as oneOfRendererEntry } from './OneOfRenderer.vue';
 import { entry as enumArrayRendererEntry } from './EnumArrayRenderer.vue';
-import { entry as categorizationEntry } from './CategorizationRenderer.vue';
-import { entry as categorizationStepperEntry } from './CategorizationStepperRenderer.vue';
 
 export const complexRenderers = [
   objectRendererEntry,
   oneOfRendererEntry,
   enumArrayRendererEntry,
-  categorizationEntry,
-  categorizationStepperEntry,
 ];
