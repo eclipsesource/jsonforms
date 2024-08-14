1157aea8f23acce809d2134945111d9bc3715578
Adam Silva <as@skeeled.com>
2024-08-14 11:52:26 +0100

    fix: matching required paths with slashes

--- packages/core/src/mappers/renderer.ts
+++ packages/core/src/mappers/renderer.ts
@@ -73,6 +73,7 @@ import {
   isVisible,
   Resolve,
   resolveSchema,
+  decode
 } from '../util';
 import {
   Translator,
@@ -114,7 +115,7 @@ const isRequired = (
   rootSchema: JsonSchema
 ): boolean => {
   const pathSegments = schemaPath.split('/');
-  const lastSegment = pathSegments[pathSegments.length - 1];
+  let lastSegment = pathSegments[pathSegments.length - 1];
   // Skip "properties", "items" etc. to resolve the parent
   const nextHigherSchemaSegments = pathSegments.slice(
     0,
@@ -126,6 +127,9 @@ const isRequired = (
     nextHigherSchemaPath,
     rootSchema
   );
+
+  // decode JSON Pointer escape sequences
+  lastSegment = decode(lastSegment);
 
   return (
     nextHigherSchema !== undefined &&
