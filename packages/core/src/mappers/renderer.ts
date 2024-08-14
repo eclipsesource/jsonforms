932d576df3f0ec8b2d43211d341bf399355a0f05
Adam Silva <as@skeeled.com>
2024-08-14 15:01:49 +0100

    refactor: simplify usage of decode in isRequired

--- packages/core/src/mappers/renderer.ts
+++ packages/core/src/mappers/renderer.ts
@@ -115,7 +115,7 @@ const isRequired = (
   rootSchema: JsonSchema
 ): boolean => {
   const pathSegments = schemaPath.split('/');
-  let lastSegment = pathSegments[pathSegments.length - 1];
+  const lastSegment = decode(pathSegments[pathSegments.length - 1]);
   // Skip "properties", "items" etc. to resolve the parent
   const nextHigherSchemaSegments = pathSegments.slice(
     0,
@@ -127,9 +127,6 @@ const isRequired = (
     nextHigherSchemaPath,
     rootSchema
   );
-
-  // decode JSON Pointer escape sequences
-  lastSegment = decode(lastSegment);
 
   return (
     nextHigherSchema !== undefined &&
