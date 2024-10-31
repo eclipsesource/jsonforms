203ed166fae098aa66d4103e59e52ca1edac56fa
Lucas Koehler <lkoehler@eclipsesource.com>
2024-10-31 15:32:46 +0100

    WIP 2371 combinator schema index heuristic
    - Adapt algorithm to determine the fitting schema index for combinators to no longer use ajv
    - New heuristic uses identifying properties that should match a const value in the schema
    
    WIP:
    - check TODO
    - add example and test
    - potentially add info to migration guide

--- packages/core/src/mappers/combinators.ts
+++ packages/core/src/mappers/combinators.ts
@@ -36,6 +36,8 @@ export interface CombinatorSubSchemaRenderInfo {
 
 export type CombinatorKeyword = 'anyOf' | 'oneOf' | 'allOf';
 
+export const COMBINATOR_TYPE_PROPERTY = 'x-jsf-type-property';
+
 export const createCombinatorRenderInfos = (
   combinatorSubSchemas: JsonSchema[],
   rootSchema: JsonSchema,
@@ -67,3 +69,92 @@ export const createCombinatorRenderInfos = (
         `${keyword}-${subSchemaIndex}`,
     };
   });
+
+/**
+ * Returns the identification property of the given data object.
+ * The following heuristics are applied:
+ * If the schema defines a `x-jsf-type-property`, it is used as the identification property.
+ * Otherwise, the first of the following properties is used:
+ * - `id`
+ * - `type`
+ * - `kind`
+ *
+ * If none of the above properties are present, the first string or number property of the data object is used.
+ */
+export const getCombinatorIdentificationProp = (
+  data: any,
+  schema: JsonSchema
+): string | undefined => {
+  // Determine the identification property
+  let idProperty: string | undefined;
+  if (
+    COMBINATOR_TYPE_PROPERTY in schema &&
+    typeof schema[COMBINATOR_TYPE_PROPERTY] === 'string'
+  ) {
+    idProperty = schema[COMBINATOR_TYPE_PROPERTY];
+  } else {
+    for (const prop of ['id', 'type', 'kind']) {
+      if (Object.prototype.hasOwnProperty.call(data, prop)) {
+        idProperty = prop;
+        break;
+      }
+    }
+  }
+
+  // If no identification property was found, use the first string or number property
+  // TODO should this iterate until it finds a property with a const in the schema?
+  if (idProperty === undefined) {
+    for (const key of Object.keys(data)) {
+      if (typeof data[key] === 'string' || typeof data[key] === 'number') {
+        idProperty = key;
+        break;
+      }
+    }
+  }
+
+  return idProperty;
+};
+
+/**
+ * @returns the index of the fitting schema or `-1` if no fitting schema was found
+ */
+export const getCombinatorIndexOfFittingSchema = (
+  data: any,
+  keyword: CombinatorKeyword,
+  schema: JsonSchema,
+  rootSchema: JsonSchema
+): number => {
+  let indexOfFittingSchema = -1;
+  const idProperty = getCombinatorIdentificationProp(data, schema);
+  if (idProperty === undefined) {
+    return indexOfFittingSchema;
+  }
+
+  for (let i = 0; i < schema[keyword]?.length; i++) {
+    let resolvedSchema = schema[keyword][i];
+    if (resolvedSchema.$ref) {
+      resolvedSchema = Resolve.schema(
+        rootSchema,
+        resolvedSchema.$ref,
+        rootSchema
+      );
+    }
+
+    // Match the identification property against a constant value in resolvedSchema
+    const maybeConstIdValue = resolvedSchema.properties?.[idProperty]?.const;
+
+    if (
+      idProperty !== undefined &&
+      maybeConstIdValue !== undefined &&
+      data[idProperty] === maybeConstIdValue
+    ) {
+      indexOfFittingSchema = i;
+      console.debug(
+        `Data matches the resolved schema for property ${idProperty}`
+      );
+      break;
+    }
+  }
+
+  return indexOfFittingSchema;
+};
