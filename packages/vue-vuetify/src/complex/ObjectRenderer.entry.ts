ef24610805b59caeef66db6dac1ffad6e3ac79e6
Krasimir Chobantonov <kchobantonov@gmail.com>
2024-12-29 21:40:02 -0500

    if the schema is an object that is selected because of mixed type then also allow such case to be handled by the object renderer

--- packages/vue-vuetify/src/complex/ObjectRenderer.entry.ts
+++ packages/vue-vuetify/src/complex/ObjectRenderer.entry.ts
@@ -1,11 +1,40 @@
 import {
+  isControl,
   isObjectControl,
   rankWith,
+  resolveSchema,
   type JsonFormsRendererRegistryEntry,
+  type JsonSchema,
+  type Scopable,
+  type TesterContext,
+  type UISchemaElement,
 } from '@jsonforms/core';
 import controlRenderer from './ObjectRenderer.vue';
+import isEmpty from 'lodash/isEmpty';
 
 export const entry: JsonFormsRendererRegistryEntry = {
   renderer: controlRenderer,
-  tester: rankWith(2, isObjectControl),
+  tester: rankWith(
+    2,
+    (
+      uischema: UISchemaElement & Scopable,
+      schema: JsonSchema,
+      context: TesterContext,
+    ) => {
+      return (
+        isObjectControl(uischema, schema, context) ||
+        // schemaMatches that is used inside the isObjectControl uses a resolve of the current schema which when the schema is provided as resolved for the MixedRender then there is no need for further resolve
+        (isControl(uischema) &&
+          !isEmpty(uischema.scope) &&
+          schema?.type === 'object' &&
+          Array.isArray(
+            resolveSchema(
+              context.rootSchema,
+              uischema.scope,
+              context.rootSchema,
+            )?.type,
+          ))
+      );
+    },
+  ),
 };
