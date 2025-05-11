5e32b15c44bd73dcd9a18d7c17a777d0211e0814
Valentin Sasek <dsl400@gmail.com>
2025-05-11 17:45:13 +0300

    allow using lazy loadable renderers

--- packages/angular/src/library/jsonforms.component.ts
+++ packages/angular/src/library/jsonforms.component.ts
@@ -24,6 +24,7 @@
 */
 import maxBy from 'lodash/maxBy';
 import {
+  ComponentFactory,
   ComponentFactoryResolver,
   Directive,
   Input,
@@ -126,11 +127,32 @@ export class JsonFormsOutlet
       renderer !== undefined &&
       renderer.tester(uischema, schema, testerContext) !== -1
     ) {
-      bestComponent = renderer.renderer;
+      if (renderer.renderer instanceof Promise) {
+        renderer.renderer.then((resolvedRenderer) => {
+          bestComponent = resolvedRenderer;
+          const componentFactory =
+            this.componentFactoryResolver.resolveComponentFactory(
+              bestComponent
+            );
+          this.renderComponent(componentFactory, uischema, schema, props);
+        });
+        return;
+      } else {
+        bestComponent = renderer.renderer;
+      }
     }
 
     const componentFactory =
       this.componentFactoryResolver.resolveComponentFactory(bestComponent);
+    this.renderComponent(componentFactory, uischema, schema, props);
+  }
+
+  private renderComponent(
+    componentFactory: ComponentFactory<any>,
+    uischema: UISchemaElement,
+    schema: JsonSchema,
+    props: JsonFormsProps
+  ) {
     this.viewContainerRef.clear();
     const currentComponentRef =
       this.viewContainerRef.createComponent(componentFactory);
