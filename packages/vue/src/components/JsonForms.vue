06e4fbf1bfe772a993419698f15048c2c5e4e134
Krasimir Chobantonov <kchobantonov@gmail.com>
2025-05-22 08:37:39 -0400

    revert adding the additionalErrors

--- packages/vue/src/components/JsonForms.vue
+++ packages/vue/src/components/JsonForms.vue
@@ -182,7 +182,6 @@ export default defineComponent({
       return {
         data: this.jsonforms.core.data,
         errors: this.jsonforms.core.errors,
-        additionalErrors: this.jsonforms.core.additionalErrors,
       };
     },
   },
