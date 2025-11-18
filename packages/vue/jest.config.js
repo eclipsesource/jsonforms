a1b8d2fc2d37124ce61ca790e04c88380d3ebe3b
Lucas Koehler <lkoehler@eclipsesource.com>
2025-11-18 16:28:42 +0100

    deps(dev): Upgrade to jest and ts-jest v29
    - Upgrade jest to ^29.7.0
      - Add now needed explicit jest-environment-jsdom where needed
      - vue: Add now needed module name mapper for @vue/test-utils
    - Upgrade ts-jest to ^29.4.5
    - vue-vanilla: Replace outdated vue-jest with @vue/vue3-jest

--- packages/vue/jest.config.js
+++ packages/vue/jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@vue/test-utils$':
+      '<rootDir>/node_modules/@vue/test-utils/dist/vue-test-utils.cjs.js',
+  },
   transform: {
     '^.+\\.ts$': 'ts-jest',
     '^.+\\.vue$': '@vue/vue3-jest',
