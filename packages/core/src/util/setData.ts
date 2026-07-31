da9df958d563a95dfb8e50a1e9074e15758f7d9c
Darius Lesch <darius.lesch@proton.me>
2026-07-31 10:51:06 +0200

    Resolve linting issue

--- packages/core/src/util/setData.ts
+++ packages/core/src/util/setData.ts
@@ -95,7 +95,9 @@ const cloneContainer = (data: any): any => {
  * properties like `__proto__`, mirroring the semantics of `resolveData`.
  */
 const ownPropertyValue = (data: any, segment: string): any =>
-  data !== null && data !== undefined && Object.prototype.hasOwnProperty.call(data, segment)
+  data !== null &&
+  data !== undefined &&
+  Object.prototype.hasOwnProperty.call(data, segment)
     ? data[segment]
     : undefined;
 
